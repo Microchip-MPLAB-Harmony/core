0b05fa57f9acc5f605e14bb92a60c0a56945e221
Leonard Elevich <leonard.elevich@microchip.com>
2017-12-01 14:13:06 -0700

    add driver only if peripheral exists

--- config/module.py
+++ config/module.py
@@ -1,7 +1,12 @@
 def loadModule():
 	print("Load Module: Harmony Drivers & System Services")
-	print("create component: Driver USART")
-	usartComponent = Module.CreateGeneratorComponent("drv_usart", "USART Driver", "/Drivers/", "driver/usart/config/usart.py")
-	usartComponent.addCapability("DRV_USART")
-	usartComponent.addDependency("USART", "USART")
+
+	if (Peripheral.moduleExists("USART")):
+		print("create component: Driver USART")
+		usartComponent = Module.CreateGeneratorComponent("drv_usart", "USART Driver", "/Drivers/", "driver/usart/config/usart.py")
+		usartComponent.addCapability("DRV_USART")
+		usartComponent.addDependency("USART", "USART")
+	else:
+		print("No USART peripheral")
+
 
