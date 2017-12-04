89dfc5d8e7bf37f71f8bef42098a2f16f39d86ac
Leonard Elevich <leonard.elevich@microchip.com>
2017-12-04 15:24:08 -0700

    added business logic

--- config/module.py
+++ config/module.py
@@ -5,7 +5,7 @@ def loadModule():
 		print("create component: Driver USART")
 		usartComponent = Module.CreateGeneratorComponent("drv_usart", "USART Driver", "/Drivers/", "driver/usart/config/usart.py")
 		usartComponent.addCapability("DRV_USART")
-		usartComponent.addDependency("USART", "USART")
+		usartComponent.addDependency("USART_Dependency", "USART")
 	else:
 		print("No USART peripheral")
 
--- driver/usart/config/usart.py
+++ driver/usart/config/usart.py
@@ -3,10 +3,16 @@ def instantiateComponent(usartComponent, index):
 	usartMenu = usartComponent.createMenuSymbol(None, None)
 	usartMenu.setLabel("USART Driver " + str(index))
 
-	useUsart = usartComponent.createBooleanSymbol("USE_USART_" + str(index), usartMenu)
-	useUsart.setLabel("Use USART " + str(index) + "?")
-	useUsart.setDescription("Enables usart instance " + str(index))
+	usartEnable = usartComponent.createBooleanSymbol("USE_USART_" + str(index), usartMenu)
+	usartEnable.setLabel("Use USART " + str(index) + "?")
+	usartEnable.setDescription("Enables usart instance " + str(index))
 
-	print("Set USART0 Config1 " + str(Database.setSymbolValue("usart0", "Config1", "Leonard", True, 1)))
+	usartBL = usartComponent.createBooleanSymbol("BL" + str(index), usartMenu)
+	usartBL.setDependencies(usartBusinessLogic, ["USE_USART_" + str(index)])
+
+
+
+def usartBusinessLogic(ChangedSym, dummy):
+	ChangedSym.getComponent().getDependencyComponent("USART_Dependency").setValue("Config1", "Leonard", True, 1)
 
 
