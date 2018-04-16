0afcca76bc469ccb3bb0810ca2542f807bd7c2d5
Settu Duraisamy <settu.duraisamy@microchip.com>
2018-04-16 20:05:58 +0530

    Removed dependency for SDHC driver

--- config/module.py
+++ config/module.py
@@ -3,7 +3,7 @@ def loadModule():
 	#define drivers and system services
 	coreComponents = [{"name":"usart", "type":"driver", "dependency":["USART"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
 					{"name":"memory", "type":"driver", "dependency":["memory_dev"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
-					{"name":"sdhc", "type":"driver", "dependency":[""], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
+					{"name":"sdhc", "type":"driver", "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
 					{"name":"fs", "type":"system", "condition":"True"},
                     {"name":"i2c", "type":"driver", "dependency":["TWIHS"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
                     {"name":"spi", "type":"driver", "dependency":["SPI"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'}]
