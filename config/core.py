5e90e5c43091937864a056f20fee1728a2aa7cce
Jeevith Kumar <jeevith.kumar@microchip.com>
2018-04-17 11:30:55 +0530

    [MODULE] Added set display name for drivers and system services MH-10036

--- config/core.py
+++ config/core.py
@@ -13,16 +13,18 @@ for coreComponent in coreComponents:
 		#create system component
 		if coreComponent['type'] == "system":
 			print("create component: " + Name.upper() + " System Service")
-			Component = Module.CreateSharedComponent("sys_" + Name, Name.upper() + " System Service", "/Harmony/System Services", "system/" + Name + "/config/sys_" + Name + ".py")
+			Component = Module.CreateSharedComponent("sys_" + Name, Name.upper(), "/Harmony/System Services", "system/" + Name + "/config/sys_" + Name + ".py")
 			Component.addCapability("sys_" + Name, "sys_" + Name)
 			if "dependency" in coreComponent:
 				for item in coreComponent['dependency']:
 					Component.addDependency("sys_" + Name + "_" + item + "_dependency", item)
+			Component.setDisplayType("System Service")
 		#create drvier component
 		else:
 			print("create component: " + Name.upper() + " Driver")
-			Component = Module.CreateGeneratorComponent("drv_" + Name, Name.upper() + " Driver", "/Harmony/Drivers/", "driver/" + Name + "/config/drv_" + Name + "_common.py", "driver/" + Name + "/config/drv_" + Name + ".py")
+			Component = Module.CreateGeneratorComponent("drv_" + Name, Name.upper(), "/Harmony/Drivers/", "driver/" + Name + "/config/drv_" + Name + "_common.py", "driver/" + Name + "/config/drv_" + Name + ".py")
 			Component.addCapability("drv_" + Name, "drv_" + Name)
+			Component.setDisplayType("Driver")
 			if "dependency" in coreComponent:
 				for item in coreComponent['dependency']:
 					Component.addDependency("drv_" + Name + "_" + item + "_dependency", item)
