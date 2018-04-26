5a485def026d7a02f1cf7d2584778c5ba3cc9936
Settu Duraisamy <settu.duraisamy@microchip.com>
2018-04-26 07:44:43 +0530

    [core] Added label and updated dependency

--- config/core.py
+++ config/core.py
@@ -6,25 +6,28 @@ harmonySystemService = Module.CreateSharedComponent("Harmony", "Harmony Core", "
 #load harmony drivers and system services from the list 
 for coreComponent in coreComponents:
 
-	#check if component should be created
-	if eval(coreComponent['condition']):
-		Name = coreComponent['name']
+    #check if component should be created
+    if eval(coreComponent['condition']):
+        Name = coreComponent['name']
+        Label = coreComponent['label']
 
-		#create system component
-		if coreComponent['type'] == "system":
-			print("create component: " + Name.upper() + " System Service")
-			Component = Module.CreateSharedComponent("sys_" + Name, Name.upper(), "/Harmony/System Services", "system/" + Name + "/config/sys_" + Name + ".py")
-			Component.addCapability("sys_" + Name, "sys_" + Name)
-			if "dependency" in coreComponent:
-				for item in coreComponent['dependency']:
-					Component.addDependency("sys_" + Name + "_" + item + "_dependency", item)
-			Component.setDisplayType("System Service")
-		#create driver component
-		else:
-			print("create component: " + Name.upper() + " Driver")
-			Component = Module.CreateGeneratorComponent("drv_" + Name, Name.upper(), "/Harmony/Drivers/", "driver/" + Name + "/config/drv_" + Name + "_common.py", "driver/" + Name + "/config/drv_" + Name + ".py")
-			Component.addCapability("drv_" + Name, "drv_" + Name)
-			Component.setDisplayType("Driver")
-			if "dependency" in coreComponent:
-				for item in coreComponent['dependency']:
-					Component.addDependency("drv_" + Name + "_" + item + "_dependency", item)
+        #create system component
+        if coreComponent['type'] == "system":
+            print("create component: " + Name.upper() + " System Service")
+            Component = Module.CreateSharedComponent("sys_" + Name, Label, "/Harmony/System Services", "system/" + Name + "/config/sys_" + Name + ".py")
+            DependencyName  = "sys_"+Name
+            Component.addCapability("sys_" + Name, DependencyName.upper())
+            if "dependency" in coreComponent:
+                for item in coreComponent['dependency']:
+                    Component.addDependency("sys_" + Name + "_" + item + "_dependency", item)
+            Component.setDisplayType("System Service")
+        #create driver component
+        else:
+            print("create component: " + Name.upper() + " Driver")
+            Component = Module.CreateGeneratorComponent("drv_" + Name, Label, "/Harmony/Drivers/", "driver/" + Name + "/config/drv_" + Name + "_common.py", "driver/" + Name + "/config/drv_" + Name + ".py")
+            DependencyName  = "drv_"+Name
+            Component.addCapability("drv_" + Name, DependencyName.upper())
+            Component.setDisplayType("Driver")
+            if "dependency" in coreComponent:
+                for item in coreComponent['dependency']:
+                    Component.addDependency("drv_" + Name + "_" + item + "_dependency", item)
--- config/module.py
+++ config/module.py
@@ -1,12 +1,12 @@
 def loadModule():
 
     #define drivers and system services
-    coreComponents = [{"name":"usart", "type":"driver", "dependency":["USART"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
-                    {"name":"memory", "type":"driver", "dependency":["MEMORY"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
-                    {"name":"sdhc", "type":"driver", "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
-                    {"name":"time", "type":"system", "dependency":["TC"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
-                    {"name":"fs", "type":"system", "condition":"True"},
-                    {"name":"i2c", "type":"driver", "dependency":["TWIHS"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
-                    {"name":"spi", "type":"driver", "dependency":["SPI"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'}]
+    coreComponents = [{"name":"usart", "label": "USART", "type":"driver", "dependency":["USART","UART"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
+                    {"name":"memory", "label": "MEMORY", "type":"driver", "dependency":["MEMORY"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
+                    {"name":"sdhc", "label":"SDHC", "type":"driver", "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
+                    {"name":"time", "label": "TIME", "type":"system", "dependency":["TC"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
+                    {"name":"fs", "label": "FILE SYSTEM", "type":"system", "condition":"True"},
+                    {"name":"i2c", "label": "I2C", "type":"driver", "dependency":["I2C"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
+                    {"name":"spi", "label": "SPI", "type":"driver", "dependency":["SPI"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'}]
     #load drivers and system services defined above
     execfile(Module.getPath() + "/config/core.py")
