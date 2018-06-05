07235c729b0fb15e73931224a833b3cebdd819e1
Jeevith Kumar <jeevith.kumar@microchip.com>
2018-06-05 11:14:55 +0530

    [DRV_USART] USART/UART double dependency is fixed.

--- config/module.py
+++ config/module.py
@@ -1,7 +1,7 @@
 def loadModule():
 
     #define drivers and system services
-    coreComponents = [{"name":"usart", "label": "USART", "type":"driver", "instance":"multi", "capability":"DRV_USART", "dependency":["USART","UART"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
+    coreComponents = [{"name":"usart", "label": "USART", "type":"driver", "instance":"multi", "capability":"DRV_USART", "dependency":["UART"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
                     {"name":"memory", "label": "MEMORY", "type":"driver", "instance":"multi", "capability":"DRV_MEMORY", "dependency":["MEMORY"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
                     {"name":"sst26", "label": "SST26", "type":"driver", "instance":"single", "capability":"MEMORY", "dependency":["QSPI"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAME70", "SAMS70", "PIC32CZ"])'},
                     {"name":"sdhc", "label":"SDHC", "type":"driver", "instance":"multi", "capability":"DRV_SDHC", "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "PIC32CZ"])'},
