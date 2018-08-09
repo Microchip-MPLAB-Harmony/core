e187651511cd43f96f473b7c05a41951aff6bfce
Settu Duraisamy <settu.duraisamy@microchip.com>
2018-08-09 06:03:29 +0530

    [SYS_TIME] Changed SYS_TIME dependency to TMR

--- config/module.py
+++ config/module.py
@@ -5,7 +5,7 @@ def loadModule():
                     {"name":"memory", "label": "MEMORY", "type":"driver", "instance":"multi", "capability":"DRV_MEMORY", "dependency":["MEMORY"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70"])'},
                     {"name":"sst26", "label": "SST26", "type":"driver", "instance":"single", "capability":"MEMORY", "dependency":["QSPI"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAME70", "SAMS70"])'},
                     {"name":"sdhc", "label":"SDHC", "type":"driver", "instance":"multi", "capability":"DRV_SDHC", "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70"])'},
-                    {"name":"time", "label": "TIME", "type":"system", "capability":"SYS_TIME", "dependency":["TC"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70"])'},
+                    {"name":"time", "label": "TIME", "type":"system", "capability":"SYS_TIME", "dependency":["TMR"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70"])'},
                     {"name":"console", "label": "CONSOLE", "type":"system", "capability":"SYS_CONSOLE", "dependency":["UART"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70"])'},
                     {"name":"fs", "label": "FILE SYSTEM", "type":"system", "capability":"SYS_FS", "condition":"True"},
                     {"name":"i2c", "label": "I2C", "type":"driver", "instance":"multi", "capability":"DRV_I2C", "dependency":["I2C"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70"])'},
