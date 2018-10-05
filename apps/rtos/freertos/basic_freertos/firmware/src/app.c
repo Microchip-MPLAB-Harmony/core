1cfbca5758a520efdcabfb3a966118616f9bf76b
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2018-10-05 20:25:48 +0530

    [FreeRTOS] Regenerated Basic FreeRTOS Demo application based on latest core
     - MH3-131
    
    Signed-off-by: Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>

---
+++ apps/rtos/freertos/basic_freertos/firmware/src/app.c
@@ -0,0 +1,173 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It
+    implements the logic of the application's state machine and it may call
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+
+    Application strings and buffers are be defined outside this structure.
+*/
+
+APP_DATA appData;
+
+/* The queue used by both tasks. */
+QueueHandle_t xQueue;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary callback functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+/* TODO:  Add any necessary local functions.
+*/
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Initialize ( void )
+{
+    /* Create the queue. */
+    xQueue = xQueueCreate( QUEUE_LENGTH, sizeof( unsigned long ) );
+
+    appData.ulValueToSend1 = 100UL;
+    appData.ulValueToSend2 = 1000UL;
+
+    /* Place the App state machine in its initial state. */
+    appData.state = APP_STATE_INIT;
+
+
+
+    /* TODO: Initialize your application's state machine and other
+     * parameters.
+     */
+}
+
+
+/******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Tasks ( void )
+{
+
+   /* Send to the queue - causing the queue receive APP2_Tasks to unblock and
+    * toggle the LED.  0 is used as the block time so the sending operation
+    * will not block - it shouldn't need to block as the queue should always
+    * be empty at this point in the code.
+    */
+    xQueueSend( xQueue, &appData.ulValueToSend1, 0U );
+
+   /* Send to the queue - causing the queue receive APP1_Tasks to unblock and
+    * toggle the LED.  0 is used as the block time so the sending operation
+    * will not block - it shouldn't need to block as the queue should always
+    * be empty at this point in the code.
+    */
+    xQueueSend( xQueue, &appData.ulValueToSend2, 0U );
+
+   /* Place this task in the blocked state until it is time to run again.
+    * The block time is specified in ticks, the constant used converts ticks
+    * to ms.  While in the Blocked state this task will not consume any CPU
+    * time.
+    */
+    vTaskDelay(QUEUE_SEND_FREQUENCY_MS );
+}
+
+
+/*******************************************************************************
+ End of File
+ */
