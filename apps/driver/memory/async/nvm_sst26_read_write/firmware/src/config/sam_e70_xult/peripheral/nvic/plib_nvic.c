5e58583302c4a5ac66b39d6831082eeffdf37723
Amit Raddi <amit.raddi@microchip.com>
2018-10-05 15:32:47 +0530

    [MEMORY] Regenerated nvm_sst26_read_write with latest changes on core
    MH3-112
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/memory/async/nvm_sst26_read_write/firmware/src/config/sam_e70_xult/peripheral/nvic/plib_nvic.c
+++ apps/driver/memory/async/nvm_sst26_read_write/firmware/src/config/sam_e70_xult/peripheral/nvic/plib_nvic.c
@@ -41,6 +41,7 @@
 #include "device.h"
 #include "plib_nvic.h"
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: NVIC Implementation
@@ -49,7 +50,7 @@
 
 void NVIC_Initialize( void )
 {
-        /* Priority 0 to 7 and no sub-priority. 0 is the highest priority */
+    /* Priority 0 to 7 and no sub-priority. 0 is the highest priority */
     NVIC_SetPriorityGrouping( 0x04 );
 
     /* Enable NVIC Controller */
@@ -60,6 +61,8 @@ void NVIC_Initialize( void )
      * from within the "Interrupt Manager" of MCC. */
     NVIC_SetPriority(EFC_IRQn, 7);
     NVIC_EnableIRQ(EFC_IRQn);
+
+
 
     return;
 }
