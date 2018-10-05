dea46c61793bdd176486c08d146b5b2d9659e546
Vishal Gandhi <vishal.gandhi@microchip.com>
2018-10-05 20:12:57 +0530

    [I2C] Regenerated I2C Sync applications MH3-132

---
+++ apps/driver/i2c/sync/i2c_multi_slave_freertos/firmware/src/app_i2c_eeprom.c
@@ -0,0 +1,206 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app_i2c_eeprom.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It
+    implements the logic of the application's state machine and it may call
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app_i2c_eeprom.h"
+#include "app_i2c_temp_sensor.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+#define APP_I2C_EEPROM_SUCCESS                                 0
+#define APP_I2C_EEPROM_ERROR                                   1
+
+/* For demonstration purpose, set the temperature sensor I2C clock speed to
+ * 100KHz and EEPROM clock speed to 400KHz
+ */
+#define APP_EEPROM_I2C_CLOCK_SPEED                  400000
+#define APP_EEPROM_START_MEMORY_ADDR                0x00
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_I2C_EEPROM_Initialize function.
+
+    Application strings and buffers are be defined outside this structure.
+*/
+
+static APP_I2C_EEPROM_DATA appEEPROMData;
+static uint8_t dummyData = 0;
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary callback functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary local functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_I2C_EEPROM_Initialize ( void )
+
+  Remarks:
+    See prototype in app_i2c_eeprom.h.
+ */
+
+void APP_I2C_EEPROM_Initialize ( void )
+{
+    /* Place the App state machine in its initial state. */
+    appEEPROMData.state = APP_I2C_EEPROM_STATE_INIT;
+    appEEPROMData.status = APP_I2C_EEPROM_ERROR;
+
+    /* TODO: Initialize your application's state machine and other
+     * parameters.
+     */
+}
+
+/******************************************************************************
+  Function:
+    void APP_I2C_EEPROM_Tasks ( void )
+
+  Remarks:
+    See prototype in app_i2c_eeprom.h.
+ */
+
+void APP_I2C_EEPROM_Tasks ( void )
+{
+    /* Check the application's current state. */
+    switch (appEEPROMData.state)
+    {
+        case APP_I2C_EEPROM_STATE_INIT:
+            /* Open I2C driver instance */
+            appEEPROMData.drvI2CHandle = DRV_I2C_Open( DRV_I2C_INDEX_0, DRV_IO_INTENT_READWRITE);
+
+            if(appEEPROMData.drvI2CHandle != DRV_HANDLE_INVALID)
+            {                
+                appEEPROMData.state = APP_I2C_EEPROM_STATE_WRITE_READ;
+            }
+            else
+            {
+                appEEPROMData.state = APP_I2C_EEPROM_STATE_ERROR;
+            }
+            break;
+
+        case APP_I2C_EEPROM_STATE_WRITE_READ:
+            /* Wait for the temperature reading to be ready */
+            OSAL_SEM_Pend( &temperatureReady, OSAL_WAIT_FOREVER );
+
+            appEEPROMData.txBuffer[0] = APP_EEPROM_START_MEMORY_ADDR;
+            appEEPROMData.txBuffer[1] = APP_TEMPERATURE_SENSOR_GetTemperature();
+
+            /* Write temperature to EEPROM */
+            if (DRV_I2C_WriteTransfer(appEEPROMData.drvI2CHandle, APP_EEPROM_AT30TSE75X_SLAVE_ADDR, (void *)appEEPROMData.txBuffer, 2) == false)
+            {
+                appEEPROMData.state = APP_I2C_EEPROM_STATE_ERROR;
+                break;
+            }
+
+            /* Poll the EEPROM status - busy bit */
+            while (DRV_I2C_WriteTransfer(appEEPROMData.drvI2CHandle, APP_EEPROM_AT30TSE75X_SLAVE_ADDR, (void *)&dummyData, 1 ) == false);
+
+            appEEPROMData.txBuffer[0] = APP_EEPROM_START_MEMORY_ADDR;
+
+            /* Read back data from EEPROM */
+            if (DRV_I2C_WriteReadTransfer(appEEPROMData.drvI2CHandle, APP_EEPROM_AT30TSE75X_SLAVE_ADDR, (void*)appEEPROMData.txBuffer, 1, (void *)appEEPROMData.rxBuffer, 1) == false)
+            {
+                appEEPROMData.state = APP_I2C_EEPROM_STATE_ERROR;
+                break;
+            }
+
+            /* Verify the read data with the data written */
+            if (appEEPROMData.rxBuffer[0] != appEEPROMData.txBuffer[1])
+            {
+                appEEPROMData.state = APP_I2C_EEPROM_STATE_ERROR;
+            }
+            else
+            {
+                /* Print the read value from EEPROM to the terminal */
+                printf("Temperature: %d\r\n", appEEPROMData.rxBuffer[0]);
+                appEEPROMData.status = APP_I2C_EEPROM_SUCCESS;
+            }
+            break;
+
+        case APP_I2C_EEPROM_STATE_ERROR:
+            appEEPROMData.status = APP_I2C_EEPROM_ERROR;
+            vTaskSuspend(NULL);
+            break;
+    }
+}
+
+/*******************************************************************************
+ End of File
+ */
---
+++ apps/driver/i2c/sync/i2c_multi_slave_freertos/firmware/src/app_i2c_temp_sensor.c
@@ -0,0 +1,208 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app_i2c_temp_sensor.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It
+    implements the logic of the application's state machine and it may call
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app_i2c_temp_sensor.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+#define APP_I2C_TEMP_STATUS_SUCCESS                                 0
+#define APP_I2C_TEMP_STATUS_ERROR                                   1
+
+/* For demonstration purpose, set the temperature sensor I2C clock speed to 
+ * 100KHz and EEPROM clock speed to 400KHz
+ */
+#define APP_TEMP_I2C_CLOCK_SPEED                    100000
+#define APP_TEMP_TEMPERATURE_REG_ADDR               0x00
+
+#define APP_TEMP_READ_RATE_MS                       1000
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_I2C_TEMP_SENSOR_Initialize function.
+
+    Application strings and buffers are be defined outside this structure.
+*/
+
+static APP_I2C_TEMP_SENSOR_DATA appTempSensorData;
+static uint8_t registerAddr;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary callback functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+/* TODO:  Add any necessary local functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    uint16_t APP_TEMPERATURE_SENSOR_GetTemperature ( void )
+
+  Remarks:
+    See prototype in app_i2c_temp_sensor.h.
+ */
+uint16_t APP_TEMPERATURE_SENSOR_GetTemperature(void)
+{
+    return appTempSensorData.temperature;
+}
+/*******************************************************************************
+  Function:
+    void APP_I2C_TEMP_SENSOR_Initialize ( void )
+
+  Remarks:
+    See prototype in app_i2c_temp_sensor.h.
+ */
+
+void APP_I2C_TEMP_SENSOR_Initialize ( void )
+{
+    /* Place the App state machine in its initial state. */
+    appTempSensorData.state = APP_I2C_TEMP_SENSOR_STATE_INIT;
+
+    /* TODO: Initialize your application's state machine and other
+     * parameters.
+     */
+    if (OSAL_SEM_Create(&temperatureReady, OSAL_SEM_TYPE_BINARY, 0, 0) == OSAL_RESULT_FALSE)
+    {
+        /* Handle error condition. Not sufficient memory to create semaphore */
+    }
+}
+
+/******************************************************************************
+  Function:
+    void APP_I2C_TEMP_SENSOR_Tasks ( void )
+
+  Remarks:
+    See prototype in app_i2c_temp_sensor.h.
+ */
+
+void APP_I2C_TEMP_SENSOR_Tasks ( void )
+{
+    /* Check the application's current state. */
+    switch (appTempSensorData.state)
+    {
+        case APP_I2C_TEMP_SENSOR_STATE_INIT:
+            /* Open I2C driver instance */
+            appTempSensorData.drvI2CHandle = DRV_I2C_Open( DRV_I2C_INDEX_0, DRV_IO_INTENT_READWRITE);
+
+            if(appTempSensorData.drvI2CHandle != DRV_HANDLE_INVALID)
+            {                
+                appTempSensorData.state = APP_I2C_TEMP_SENSOR_STATE_READ_SENSOR;
+            }
+            else
+            {
+                appTempSensorData.state = APP_I2C_TEMP_SENSOR_STATE_ERROR;
+            }
+            break;
+
+        case APP_I2C_TEMP_SENSOR_STATE_READ_SENSOR:
+
+            /* Read temperature readings every 1000 ms */
+            vTaskDelay(APP_TEMP_READ_RATE_MS/portTICK_PERIOD_MS);
+
+            registerAddr = APP_TEMP_TEMPERATURE_REG_ADDR;
+
+            /* Read temperature reading */
+            if (DRV_I2C_WriteReadTransfer(appTempSensorData.drvI2CHandle, APP_TEMP_AT30TSE75X_SLAVE_ADDR, (void*)&registerAddr, 1, (void *)appTempSensorData.rxBuffer, 2 ) == true)
+            {
+                uint16_t temp;
+                temp = (appTempSensorData.rxBuffer[0] << 8) | appTempSensorData.rxBuffer[1];
+                temp = (temp >> 7) * 0.5;
+                appTempSensorData.temperature = (uint8_t)temp;
+
+                appTempSensorData.status = APP_I2C_TEMP_STATUS_SUCCESS;
+                OSAL_SEM_Post(&temperatureReady);
+            }
+            else
+            {
+                appTempSensorData.state = APP_I2C_TEMP_SENSOR_STATE_ERROR;
+            }
+            break;
+
+        case APP_I2C_TEMP_SENSOR_STATE_ERROR:
+            appTempSensorData.status = APP_I2C_TEMP_STATUS_ERROR;
+            /* Allow other threads to run */
+            vTaskSuspend(NULL);
+            break;
+    }
+}
+
+/*******************************************************************************
+ End of File
+ */
