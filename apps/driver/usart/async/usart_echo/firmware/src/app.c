bc8d505aec7d39d9040acb3244d3ceab96e88362
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2018-10-05 17:28:29 +0530

    [USART] Regenerated usart_echo async demo application based on latest core  - MH3-129
    Signed-off-by: Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>

---
+++ apps/driver/usart/async/usart_echo/firmware/src/app.c
@@ -0,0 +1,248 @@
+/*******************************************************************************
+  MPLAB Harmony Application Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app.c
+
+  Summary:
+    This file contains the source code for the MPLAB Harmony application.
+
+  Description:
+    This file contains the source code for the MPLAB Harmony application.  It
+    implements the logic of the application's state machine and it may call
+    API routines of other MPLAB Harmony modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include "app.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+
+    Application strings and buffers are be defined outside this structure.
+*/
+
+APP_DATA appData;
+
+#define APP_DATA_SIZE   1
+
+static char messageStart[] = "**** USART Driver Echo Demo Application ****\r\n\
+**** Type a character and observe it echo back ***\r\n\
+**** LED toggles on each time the character is echoed ***\r\n";
+static char readBuffer[APP_DATA_SIZE] = {};
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+void APP_BufferEventHandler(DRV_USART_BUFFER_EVENT bufferEvent, DRV_USART_BUFFER_HANDLE bufferHandle, uintptr_t context )
+{
+    switch(bufferEvent)
+    {
+        case DRV_USART_BUFFER_EVENT_COMPLETE:
+        {
+            appData.completeStatus = true;
+            break;
+        }
+
+        case DRV_USART_BUFFER_EVENT_ERROR:
+        {
+            appData.errorStatus = true;
+            break;
+        }
+
+        default:
+        {
+            break;
+        }
+    }
+}
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+/* TODO:  Add any necessary local functions.
+*/
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Initialize ( void )
+{
+    /* Place the App state machine in its initial state. */
+    LED1_Off();
+    appData.state = APP_STATE_INIT;
+    appData.prevState = APP_STATE_INIT;
+    appData.usartHandle = DRV_HANDLE_INVALID;
+    appData.messageBufHandler = DRV_USART_BUFFER_HANDLE_INVALID;
+    appData.writeBufHandler = DRV_USART_BUFFER_HANDLE_INVALID;
+    appData.readBufHandler = DRV_USART_BUFFER_HANDLE_INVALID;
+}
+
+
+/******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Tasks ( void )
+{
+
+    /* Check the application's current state. */
+    switch ( appData.state )
+    {
+        /* Application's initial state. */
+        case APP_STATE_INIT:
+        {
+            appData.usartHandle = DRV_USART_Open(DRV_USART_INDEX_0, DRV_IO_INTENT_READWRITE);
+
+            if (appData.usartHandle != DRV_HANDLE_INVALID)
+            {
+                DRV_USART_BufferEventHandlerSet(appData.usartHandle, APP_BufferEventHandler, 0);
+                appData.state = APP_STATE_SEND_MESSGE;
+            }
+            break;
+        }
+
+        case APP_STATE_SEND_MESSGE:
+        {
+            DRV_USART_WriteBufferAdd(appData.usartHandle, messageStart, sizeof(messageStart), &appData.messageBufHandler);
+            appData.prevState = APP_STATE_SEND_MESSGE;
+            appData.state = APP_STATE_WAIT;
+            break;
+        }
+
+        case APP_STATE_RECEIVE_BUFFER:
+        {
+            DRV_USART_ReadBufferAdd(appData.usartHandle, readBuffer, APP_DATA_SIZE, &appData.readBufHandler);
+            appData.prevState = APP_STATE_RECEIVE_BUFFER;
+            appData.state = APP_STATE_WAIT;
+            break;
+        }
+
+        case APP_STATE_SEND_BUFFER:
+        {
+            DRV_USART_WriteBufferAdd(appData.usartHandle, readBuffer, APP_DATA_SIZE, &appData.writeBufHandler);
+            appData.prevState = APP_STATE_SEND_BUFFER;
+            appData.state = APP_STATE_WAIT;
+            break;
+        }
+
+        case APP_STATE_WAIT:
+        {
+            if(appData.completeStatus == true)
+            {
+                appData.completeStatus = false;
+
+                if(appData.prevState == APP_STATE_SEND_MESSGE || appData.prevState == APP_STATE_SEND_BUFFER)
+                {
+                    LED1_Toggle();
+                    appData.state = APP_STATE_RECEIVE_BUFFER;
+                }
+                else if(appData.prevState == APP_STATE_RECEIVE_BUFFER)
+                {
+                    appData.state = APP_STATE_SEND_BUFFER;
+                }
+
+                appData.prevState = APP_STATE_WAIT;
+            }
+            else if(appData.errorStatus == true)
+            {
+                appData.errorStatus = false;
+                appData.prevState = APP_STATE_WAIT;
+                appData.state = APP_STATE_IDLE;
+            }
+
+            break;
+        }
+
+        case APP_STATE_IDLE:
+        {
+            break;
+        }
+
+        /* The default state should never be executed. */
+        default:
+        {
+            break;
+        }
+    }
+}
+
+
+/*******************************************************************************
+ End of File
+ */
