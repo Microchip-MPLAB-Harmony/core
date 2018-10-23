5a4c885eb8aac6271b80db8eaf45819748361541
Vishal Gandhi <vishal.gandhi@microchip.com>
2018-10-23 13:48:03 +0530

    [SDSPI] Created sdspi_fat application for SAM C21N and SAM E70 MH3-1385

---
+++ apps/fs/sdspi_fat/sync/firmware/src/config/sam_e70_xult_freertos/initialization.c
@@ -0,0 +1,242 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="DRV_SDSPI Instance 0 Initialization Data">
+
+/* SD Card Client Objects Pool */
+DRV_SDSPI_CLIENT_OBJ drvSDSPI0ClientObjPool[DRV_SDSPI_CLIENTS_NUMBER_IDX0] = {0};
+
+/* SPI PLIB Interface Initialization for SDSPI Driver */
+DRV_SDSPI_PLIB_INTERFACE drvSDSPI0PlibAPI = {
+
+    /* SPI PLIB WriteRead function */
+    .writeRead = (DRV_SDSPI_WRITEREAD)SPI0_WriteRead,
+
+    /* SPI PLIB Write function */
+    .write = (DRV_SDSPI_WRITE)SPI0_Write,
+
+    /* SPI PLIB Read function */
+    .read = (DRV_SDSPI_READ)SPI0_Read,
+
+    /* SPI PLIB Transfer Status function */
+    .isBusy = (DRV_SDSPI_IS_BUSY)SPI0_IsBusy,
+
+    .transferSetup = (DRV_SDSPI_SETUP)SPI0_TransferSetup,
+
+    /* SPI PLIB Callback Register */
+    .callbackRegister = (DRV_SDSPI_CALLBACK_REGISTER)SPI0_CallbackRegister,
+};
+
+uint32_t drvSDSPI0remapDataBits[]= { 0x0, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80 };
+uint32_t drvSDSPI0remapClockPolarity[] = { 0x0, 0x1 };
+uint32_t drvSDSPI0remapClockPhase[] = { 0x0, 0x2 };
+/* SDSPI Driver Initialization Data */
+DRV_SDSPI_INIT drvSDSPI0InitData =
+{
+    /* SD Card SPI PLIB API interface*/
+    .spiPlib            = &drvSDSPI0PlibAPI,
+
+    .remapDataBits = drvSDSPI0remapDataBits,
+
+    .remapClockPolarity = drvSDSPI0remapClockPolarity,
+
+    .remapClockPhase = drvSDSPI0remapClockPhase,
+
+    /* SDSPI Number of clients */
+    .numClients         = DRV_SDSPI_CLIENTS_NUMBER_IDX0,
+
+    /* SDSPI Client Objects Pool */
+    .clientObjPool      = (uintptr_t)&drvSDSPI0ClientObjPool[0],
+
+    .chipSelectPin      = DRV_SDSPI_CHIP_SELECT_PIN_IDX0,
+
+    .sdcardSpeedHz      = DRV_SDSPI_SPEED_HZ_IDX0,
+
+    .writeProtectPin    = SYS_PORT_PIN_NONE,
+
+    .isRegisterWithFS   = DRV_SDSPI_REGISTER_WITH_FS_IDX0,
+
+    /* DMA Channel for Transmit */
+    .txDMAChannel = DRV_SDSPI_XMIT_DMA_CH_IDX0,
+
+    /* DMA Channel for Receive */
+    .rxDMAChannel  = DRV_SDSPI_RCV_DMA_CH_IDX0,
+
+    /* SPI Transmit Register */
+    .txAddress = (void *)&(SPI0_REGS->SPI_TDR),
+
+    /* SPI Receive Register */
+    .rxAddress  = (void *)&(SPI0_REGS->SPI_RDR),
+};
+
+// </editor-fold>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+/*** File System Initialization Data ***/
+
+
+const SYS_FS_MEDIA_MOUNT_DATA sysfsMountTable[SYS_FS_VOLUME_NUMBER] = 
+{
+	{NULL}
+};
+
+
+
+
+const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
+{
+    {
+        .nativeFileSystemType = FAT,
+        .nativeFileSystemFunctions = &FatFsFunctions
+    }
+};
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="SYS_TIME Initialization Data">
+
+TIME_PLIB_API sysTimePlibAPI = {
+    .timerCallbackSet = (TIME_CallbackSet)TC0_CH0_TimerCallbackRegister,
+    .timerCounterGet = (TIME_CounterGet)TC0_CH0_TimerCounterGet,
+     .timerPeriodSet = (TIME_PeriodSet)TC0_CH0_TimerPeriodSet,
+    .timerFrequencyGet = (TIME_FrequencyGet)TC0_CH0_TimerFrequencyGet,
+    .timerCompareSet = (TIME_CompareSet)TC0_CH0_TimerCompareSet,
+    .timerStart = (TIME_Start)TC0_CH0_TimerStart,
+    .timerStop = (TIME_Stop)TC0_CH0_TimerStop 
+};
+
+SYS_TIME_INIT sysTimeInitData =
+{
+    .timePlib = &sysTimePlibAPI,
+    .hwTimerIntNum = TC0_CH0_IRQn,
+};
+
+// </editor-fold>
+
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+    CLK_Initialize();
+	PIO_Initialize();
+
+
+    NVIC_Initialize();
+    XDMAC_Initialize();
+
+	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
+
+	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
+
+	BSP_Initialize();
+	SPI0_Initialize();
+
+ 
+    TC0_CH0_TimerInitialize(); 
+     
+    
+
+    /* Initialize SDSPI0 Driver Instance */
+    sysObj.drvSDSPI0 = DRV_SDSPI_Initialize(DRV_SDSPI_INDEX_0, (SYS_MODULE_INIT *)&drvSDSPI0InitData);
+
+    sysObj.sysTime = SYS_TIME_Initialize(SYS_TIME_INDEX_0, (SYS_MODULE_INIT *)&sysTimeInitData);
+
+    /*** File System Service Initialization Code ***/
+    SYS_FS_Initialize( (const void *) sysFSInit );
+
+
+    APP_Initialize();
+
+
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
