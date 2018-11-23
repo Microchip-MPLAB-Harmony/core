27099c60fd771689a843201d4024d4d164a6a7bb
Amit Raddi <amit.raddi@microchip.com>
2018-11-23 12:22:02 +0530

    [config] Modified module.py to support SDCARD Driver
    MH3-1885
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- config/module.py
+++ config/module.py
@@ -31,13 +31,12 @@ def loadModule():
                     {"name":"usart", "label": "USART", "type":"driver", "instance":"multi", "capability":["DRV_USART"], "dependency":["UART"], "condition":"True"},
                     {"name":"memory", "label": "MEMORY", "type":"driver", "instance":"multi", "capability":["DRV_MEDIA"], "capability_type":"multi", "dependency":["MEMORY"], "condition":"True"},
                     {"name":"sst26", "label": "SST26", "type":"driver", "instance":"single", "capability":["MEMORY"], "dependency":["QSPI"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAME70", "SAMS70"])'},
-                    {"name":"sdhc", "label":"SDHC", "type":"driver", "instance":"multi", "capability":["DRV_MEDIA"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70"])'},
                     {"name":"i2c", "label": "I2C", "type":"driver", "instance":"multi", "capability":["DRV_I2C"], "dependency":["I2C"], "condition":"True"},
                     {"name":"spi", "label": "SPI", "type":"driver", "instance":"multi", "capability":["DRV_SPI"], "dependency":["SPI"], "condition":"True"},
                     {"name":"i2s", "label": "I2S", "type":"driver", "instance":"multi", "capability":["DRV_I2S"], "dependency":["I2S"], "condition":'any(x in Variables.get("__PROCESSOR") for x in ["SAMV70", "SAMV71", "SAME70", "SAMS70", "SAMD21"])'},
                     {"name":"at24", "label": "AT24", "type":"driver", "instance":"single", "capability":["MEMORY"], "dependency":["I2C"], "condition":"True"},
                     {"name":"at25", "label": "AT25", "type":"driver", "instance":"single", "capability":["MEMORY"], "dependency":["SPI"], "condition":"True"},
-                    {"name":"sdspi", "label": "SD Card (SPI)", "type":"driver", "instance":"multi", "capability":["DRV_MEDIA"], "dependency":["SPI"], "condition":"True"}
+                    {"name":"sdcard", "label": "SDCARD", "type":"driver", "instance":"single", "capability":["DRV_MEDIA"], "dependency":["SPI"], "condition":"True"},
                     ]
     #load drivers and system services defined above
     execfile(Module.getPath() + "/config/core.py")
