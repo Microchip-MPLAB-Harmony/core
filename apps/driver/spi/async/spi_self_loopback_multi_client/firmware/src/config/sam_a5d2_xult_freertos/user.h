795dc9252aa3e55302ffe139995d58d429dda0fa
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2018-11-27 11:32:42 +0530

    [SPI] Added spi_self_loopback_multi_client freertos demo
    Added spi_self_loopback_multi_client freertos IAR demo for Asynchronous driver on SAMA5D2.
    
    MH3-2025

---
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_a5d2_xult_freertos/user.h
@@ -0,0 +1,51 @@
+/*******************************************************************************
+  User Configuration Header
+
+  File Name:
+    user.h
+
+  Summary:
+    Build-time configuration header for the user defined by this project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    It only provides macro definitions for build-time configuration options
+
+*******************************************************************************/
+
+#ifndef USER_H
+#define USER_H
+
+#include <stdint.h>
+#include "peripheral/l2cc/plib_l2cc.h"
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: User Configuration macros
+// *****************************************************************************
+// *****************************************************************************
+#define LED_On()                    LED1_On()
+#define LED_Off()                   LED1_Off()
+#define LED_Toggle()                LED1_Toggle()
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // USER_H
+/*******************************************************************************
+ End of File
+*/
