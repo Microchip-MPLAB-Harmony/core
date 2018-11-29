1baaee16de39fe143f929903452c438b8f0c195f
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2018-11-29 15:11:27 +0530

    [FreeRTOS] Regenerated and Tested task_notification_freertos_sam_v71_xult demo application
    MH3-3629
    
    Signed-off-by: Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>

--- apps/rtos/freertos/task_notification_freertos/firmware/src/config/sam_v71_xult/tasks.c
+++ apps/rtos/freertos/task_notification_freertos/firmware/src/config/sam_v71_xult/tasks.c
@@ -58,6 +58,9 @@
 // Section: RTOS "Tasks" Routine
 // *****************************************************************************
 // *****************************************************************************
+/* Handle for the APP_Tasks. */
+TaskHandle_t xAPP_Tasks;
+
 void _APP_Tasks(  void *pvParameters  )
 {
     while(1)
@@ -65,6 +68,9 @@ void _APP_Tasks(  void *pvParameters  )
         APP_Tasks();
     }
 }
+/* Handle for the APP1_Tasks. */
+TaskHandle_t xAPP1_Tasks;
+
 void _APP1_Tasks(  void *pvParameters  )
 {
     while(1)
