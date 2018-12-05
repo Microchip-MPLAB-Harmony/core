d52624e6d5ad97a8bef9167654240221b156a62f
Amit Raddi <amit.raddi@microchip.com>
2018-12-05 14:12:53 +0530

    [AT24] Regenerated and Tested at24_eeprom_read_write_sam_v71_xult
    MH3-3485
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/at24/at24_eeprom_read_write/firmware/src/config/sam_v71_xult/user.h
+++ apps/driver/at24/at24_eeprom_read_write/firmware/src/config/sam_v71_xult/user.h
@@ -43,6 +43,8 @@
 #ifndef USER_H
 #define USER_H
 
+#include "bsp/bsp.h"
+
 // DOM-IGNORE-BEGIN
 #ifdef __cplusplus  // Provide C++ Compatibility
 
@@ -56,9 +58,9 @@ extern "C" {
 // Section: User Configuration macros
 // *****************************************************************************
 // *****************************************************************************
-#define LED_On()                    LED0_On()
-#define LED_Off()                   LED0_Off()
-#define LED_Toggle()                LED0_Toggle()
+#define LED_ON()                    LED0_On()
+#define LED_OFF()                   LED0_Off()
+#define LED_TOGGLE()                LED0_Toggle()
 
 //DOM-IGNORE-BEGIN
 #ifdef __cplusplus
