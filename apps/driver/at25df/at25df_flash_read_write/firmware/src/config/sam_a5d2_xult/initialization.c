06621b8dff3c8a8e0e2c170fb289572124764432
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2018-12-17 19:54:26 +0530

    [AT25DF] Added at25df_flash_read_write demo
    Added at25df_flash_read_write IAR demo for SPI Flash on SAMA5D2.
    
    MH3-4116

---
+++ apps/driver/at25df/at25df_flash_read_write/firmware/src/config/sam_a5d2_xult/initialization.c
@@ -0,0 +1,172 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="DRV_AT25DF Initialization Data">
+
+/* SPI PLIB Interface Initialization for AT25DF Driver */
+const DRV_AT25DF_PLIB_INTERFACE drvAT25DFPlibAPI = {
+
+    /* SPI PLIB WriteRead function */
+    .writeRead = (DRV_AT25DF_PLIB_WRITE_READ)SPI0_WriteRead,
+
+    /* SPI PLIB Write function */
+    .write = (DRV_AT25DF_PLIB_WRITE)SPI0_Write,
+
+    /* SPI PLIB Read function */
+    .read = (DRV_AT25DF_PLIB_READ)SPI0_Read,
+
+    /* SPI PLIB Transfer Status function */
+    .isBusy = (DRV_AT25DF_PLIB_IS_BUSY)SPI0_IsBusy,
+
+    /* SPI PLIB Callback Register */
+    .callbackRegister = (DRV_AT25DF_PLIB_CALLBACK_REGISTER)SPI0_CallbackRegister,
+};
+
+/* AT25DF Driver Initialization Data */
+const DRV_AT25DF_INIT drvAT25DFInitData =
+{
+    /* SPI PLIB API  interface*/
+    .spiPlib = &drvAT25DFPlibAPI,
+
+    /* AT25DF Number of clients */
+    .numClients = DRV_AT25DF_CLIENTS_NUMBER_IDX,
+
+    /* FLASH Page Size in bytes */
+    .pageSize = DRV_AT25DF_PAGE_SIZE,
+
+    /* Total size of the FLASH in bytes */
+    .flashSize = DRV_AT25DF_FLASH_SIZE,
+
+    .blockStartAddress =    0x0,
+
+    .chipSelectPin    = DRV_AT25DF_CHIP_SELECT_PIN_IDX
+};
+
+// </editor-fold>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+    CLK_Initialize();
+	PIO_Initialize();
+
+
+	BSP_Initialize();
+	PIT_TimerInitialize();
+
+    MMU_Initialize();
+    Matrix_Initialize();
+
+    PLIB_L2CC_Initialize();
+
+    INT_Initialize();
+	WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk; 		// Disable WDT 
+
+	SPI0_Initialize();
+
+
+    sysObj.drvAT25DF = DRV_AT25DF_Initialize(DRV_AT25DF_INDEX, (SYS_MODULE_INIT *)&drvAT25DFInitData);
+
+
+
+
+    APP_Initialize();
+
+
+}
+
+
+/*******************************************************************************
+ End of File
+*/
+
