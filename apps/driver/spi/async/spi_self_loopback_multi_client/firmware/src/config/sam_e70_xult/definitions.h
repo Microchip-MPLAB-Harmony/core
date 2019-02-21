32895a1a5d0faef1956694ffa4b0fb1dff90d33b
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-02-21 23:48:31 +0530

    [SPI] Regenerated and tested async_spi_self_loopback_multi_client_sam_e70_xult
    MH3-6610
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_e70_xult/definitions.h
@@ -56,6 +56,7 @@
 #include "driver/spi/drv_spi.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
+#include "system/cache/sys_cache.h"
 #include "system/dma/sys_dma.h"
 #include "osal/osal.h"
 #include "app_client1.h"
