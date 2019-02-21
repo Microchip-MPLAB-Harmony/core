27504f64c132126fea43300071429be432e09a6d
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-02-22 00:50:27 +0530

    [SPI] Regenerated and tested sync_spi_multi_slave_sam_e54_xpro_freertos
    MH3-6622
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/driver/spi/sync/spi_multi_slave/firmware/src/config/sam_e54_xpro_freertos/initialization.c
+++ apps/driver/spi/sync/spi_multi_slave/firmware/src/config/sam_e54_xpro_freertos/initialization.c
@@ -106,7 +106,7 @@
 // <editor-fold defaultstate="collapsed" desc="DRV_SPI Instance 0 Initialization Data">
 
 /* SPI Client Objects Pool */
-static DRV_SPI_CLIENT_OBJ drvSPI0ClientObjPool[DRV_SPI_CLIENTS_NUMBER_IDX0] = {0};
+static DRV_SPI_CLIENT_OBJ drvSPI0ClientObjPool[DRV_SPI_CLIENTS_NUMBER_IDX0];
 
 /* SPI PLIB Interface Initialization */
 const DRV_SPI_PLIB_INTERFACE drvSPI0PlibAPI = {
@@ -213,8 +213,6 @@ void SYS_Initialize ( void* data )
     DMAC_Initialize();
 
 
-    NVIC_Initialize();
-
     /* Initialize SPI0 Driver Instance */
     sysObj.drvSPI0 = DRV_SPI_Initialize(DRV_SPI_INDEX_0, (SYS_MODULE_INIT *)&drvSPI0InitData);
 
@@ -225,7 +223,8 @@ void SYS_Initialize ( void* data )
     APP_EEPROM2_Initialize();
 
 
-  
+    NVIC_Initialize();
+
 }
 
 
