ada50fa35fc039571600597f8dfa8e43c5693429
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-02-21 23:58:48 +0530

    [SPI] Regenerated and tested async_spi_self_loopback_multi_client_sam_v71_xult
    MH3-6614
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_v71_xult/configuration.h
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_v71_xult/configuration.h
@@ -71,10 +71,8 @@ extern "C" {
 // Section: System Configuration
 // *****************************************************************************
 // *****************************************************************************
-#define DCACHE_CLEAN_BY_ADDR(data, size)       SCB_CleanDCache_by_Addr((uint32_t *)data, size)
-#define DCACHE_INVALIDATE_BY_ADDR(data, size)  SCB_InvalidateDCache_by_Addr((uint32_t *)data, size)
 
-#define DATA_CACHE_ENABLED                     true
+
 
 // *****************************************************************************
 // *****************************************************************************
@@ -91,8 +89,6 @@ extern "C" {
 /* SPI Driver Instance 0 Configuration Options */
 #define DRV_SPI_INDEX_0                       0
 #define DRV_SPI_CLIENTS_NUMBER_IDX0           2
-
-#define DRV_SPI_INT_SRC_IDX0                  SPI0_IRQn
 #define DRV_SPI_QUEUE_SIZE_IDX0               4
 
 /* SPI Driver Common Configuration Options */
