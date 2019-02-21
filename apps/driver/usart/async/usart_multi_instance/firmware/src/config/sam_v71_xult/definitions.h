1cc47c8c74ccbf66bc910917bd93eeeec49dc92e
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-02-21 17:42:41 +0530

    [USART] Regenerated async/usart_multi_instance demo for sam_v71_xult configuration
    MH3-6765

--- apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_v71_xult/definitions.h
@@ -48,7 +48,6 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
-#include "driver/usart/drv_usart_definitions.h"
 #include "driver/usart/drv_usart.h"
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
@@ -57,6 +56,7 @@
 #include "bsp/bsp.h"
 #include "peripheral/usart/plib_usart0.h"
 #include "system/int/sys_int.h"
+#include "system/cache/sys_cache.h"
 #include "system/dma/sys_dma.h"
 #include "osal/osal.h"
 #include "app1.h"
