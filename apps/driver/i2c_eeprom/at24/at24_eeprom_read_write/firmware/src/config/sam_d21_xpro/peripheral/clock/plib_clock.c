7cd1ff31127e3e91876e8580e53d8fdfc7a3c1e0
Mohan RP <mohan.kumar@microchip.com>
2019-02-21 11:30:51 +0530

    [AT24] Regenerated and tested driver_i2c_eeprom_at24_at24_eeprom_read_write_sam_d21_xpro
    MH3-6540

--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_d21_xpro/peripheral/clock/plib_clock.c
@@ -135,7 +135,7 @@ void CLOCK_Initialize (void)
 
     /* Selection of the Generator and write Lock for SERCOM2_CORE */
     GCLK_REGS->GCLK_CLKCTRL = GCLK_CLKCTRL_ID(22) | GCLK_CLKCTRL_GEN(0x0)  | GCLK_CLKCTRL_CLKEN_Msk;
-
+    
     /* Configure the APBC Bridge Clocks */
     PM_REGS->PM_APBCMASK = 0x10010;
 
