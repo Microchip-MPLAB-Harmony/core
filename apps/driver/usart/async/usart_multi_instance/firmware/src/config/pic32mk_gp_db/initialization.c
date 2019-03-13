7dbacd713a82ccc3d7f9c1ecf4c11e879e87e14d
Mohan RP <mohan.kumar@microchip.com>
2019-03-13 08:08:46 +0530

    [USART] Created async_usart_multi_instance application for pic32mk
    MH3-8786

---
+++ apps/driver/usart/async/usart_multi_instance/firmware/src/config/pic32mk_gp_db/initialization.c
@@ -0,0 +1,346 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+/*** DEVCFG0 ***/
+#pragma config DEBUG =      OFF
+#pragma config JTAGEN =     OFF
+#pragma config ICESEL =     ICS_PGx1
+#pragma config TRCEN =      OFF
+#pragma config BOOTISA =    MIPS32
+#pragma config FSLEEP =     OFF
+#pragma config DBGPER =     PG_ALL
+#pragma config SMCLR =      MCLR_NORM
+#pragma config SOSCGAIN =   GAIN_2X
+#pragma config SOSCBOOST =  ON
+#pragma config POSCGAIN =   GAIN_LEVEL_3
+#pragma config POSCBOOST =  ON
+#pragma config EJTAGBEN =   NORMAL
+
+/*** DEVCFG1 ***/
+#pragma config FNOSC =      SPLL
+#pragma config DMTINTV =    WIN_127_128
+#pragma config FSOSCEN =    ON
+#pragma config IESO =       ON
+#pragma config POSCMOD =    OFF
+#pragma config OSCIOFNC =   OFF
+#pragma config FCKSM =      CSECME
+#pragma config WDTPS =      PS1048576
+#pragma config WDTSPGM =    STOP
+#pragma config FWDTEN =     OFF
+#pragma config WINDIS =     NORMAL
+#pragma config FWDTWINSZ =  WINSZ_25
+#pragma config DMTCNT =     DMT31
+#pragma config FDMTEN =     OFF
+
+/*** DEVCFG2 ***/
+#pragma config FPLLIDIV =   DIV_1
+#pragma config FPLLRNG =    RANGE_5_10_MHZ
+#pragma config FPLLICLK =   PLL_FRC
+#pragma config FPLLMULT =   MUL_60
+#pragma config FPLLODIV =   DIV_4
+#pragma config VBATBOREN =  ON
+#pragma config DSBOREN =    ON
+#pragma config DSWDTPS =    DSPS32
+#pragma config DSWDTOSC =   LPRC
+#pragma config DSWDTEN =    OFF
+#pragma config FDSEN =      ON
+#pragma config BORSEL =     HIGH
+#pragma config UPLLEN =     OFF
+
+/*** DEVCFG3 ***/
+#pragma config USERID =     0xffff
+#pragma config FUSBIDIO2 =   ON
+#pragma config FVBUSIO2 =  ON
+#pragma config PGL1WAY =    ON
+#pragma config PMDL1WAY =   ON
+#pragma config IOL1WAY =    ON
+#pragma config FUSBIDIO1 =   ON
+#pragma config FVBUSIO1 =  ON
+
+/*** BF1SEQ ***/
+#pragma config TSEQ =       0x0
+#pragma config CSEQ =       0xffff
+
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="DRV_USART Instance 1 Initialization Data">
+
+static DRV_USART_CLIENT_OBJ drvUSART1ClientObjPool[DRV_USART_CLIENTS_NUMBER_IDX1];
+
+/* USART transmit/receive transfer objects pool */
+static DRV_USART_BUFFER_OBJ drvUSART1BufferObjPool[DRV_USART_QUEUE_SIZE_IDX1];
+
+const DRV_USART_PLIB_INTERFACE drvUsart1PlibAPI = {
+    .readCallbackRegister = (DRV_USART_PLIB_READ_CALLBACK_REG)UART4_ReadCallbackRegister,
+    .read = (DRV_USART_PLIB_READ)UART4_Read,
+    .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)UART4_ReadIsBusy,
+    .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)UART4_ReadCountGet,
+    .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)UART4_WriteCallbackRegister,
+    .write = (DRV_USART_PLIB_WRITE)UART4_Write,
+    .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)UART4_WriteIsBusy,
+    .writeCountGet = (DRV_USART_PLIB_WRITE_COUNT_GET)UART4_WriteCountGet,
+    .errorGet = (DRV_USART_PLIB_ERROR_GET)UART4_ErrorGet,
+    .serialSetup = (DRV_USART_PLIB_SERIAL_SETUP)UART4_SerialSetup
+};
+
+const uint32_t drvUsart1remapDataWidth[] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x0, 0x6 };
+const uint32_t drvUsart1remapParity[] = { 0x0, 0x2, 0x4, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+const uint32_t drvUsart1remapStopBits[] = { 0x0, 0xFFFFFFFF, 0x1 };
+const uint32_t drvUsart1remapError[] = { 0x2, 0x8, 0x4 };
+
+const DRV_USART_INTERRUPT_SOURCES drvUSART1InterruptSources =
+{
+    /* Peripheral has more than one interrupt vector */
+    .isSingleIntSrc                        = false,
+
+    /* Peripheral interrupt lines */
+    .intSources.multi.usartTxCompleteInt   = _UART4_TX_VECTOR,
+    .intSources.multi.usartTxReadyInt      = -1,
+    .intSources.multi.usartRxCompleteInt   = _UART4_RX_VECTOR,
+    .intSources.multi.usartErrorInt        = _UART4_FAULT_VECTOR,
+};
+
+const DRV_USART_INIT drvUsart1InitData =
+{
+    .usartPlib = &drvUsart1PlibAPI,
+
+    /* USART Number of clients */
+    .numClients = DRV_USART_CLIENTS_NUMBER_IDX1,
+
+    /* USART Client Objects Pool */
+    .clientObjPool = (uintptr_t)&drvUSART1ClientObjPool[0],
+
+    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
+
+    .dmaChannelReceive = SYS_DMA_CHANNEL_NONE,
+
+    /* Combined size of transmit and receive buffer objects */
+    .bufferObjPoolSize = DRV_USART_QUEUE_SIZE_IDX1,
+
+    /* USART transmit and receive buffer buffer objects pool */
+    .bufferObjPool = (uintptr_t)&drvUSART1BufferObjPool[0],
+
+    .interruptSources = &drvUSART1InterruptSources,
+
+    .remapDataWidth = drvUsart1remapDataWidth,
+
+    .remapParity = drvUsart1remapParity,
+
+    .remapStopBits = drvUsart1remapStopBits,
+
+    .remapError = drvUsart1remapError,
+};
+
+// </editor-fold>
+// <editor-fold defaultstate="collapsed" desc="DRV_USART Instance 0 Initialization Data">
+
+static DRV_USART_CLIENT_OBJ drvUSART0ClientObjPool[DRV_USART_CLIENTS_NUMBER_IDX0];
+
+/* USART transmit/receive transfer objects pool */
+static DRV_USART_BUFFER_OBJ drvUSART0BufferObjPool[DRV_USART_QUEUE_SIZE_IDX0];
+
+const DRV_USART_PLIB_INTERFACE drvUsart0PlibAPI = {
+    .readCallbackRegister = (DRV_USART_PLIB_READ_CALLBACK_REG)UART6_ReadCallbackRegister,
+    .read = (DRV_USART_PLIB_READ)UART6_Read,
+    .readIsBusy = (DRV_USART_PLIB_READ_IS_BUSY)UART6_ReadIsBusy,
+    .readCountGet = (DRV_USART_PLIB_READ_COUNT_GET)UART6_ReadCountGet,
+    .writeCallbackRegister = (DRV_USART_PLIB_WRITE_CALLBACK_REG)UART6_WriteCallbackRegister,
+    .write = (DRV_USART_PLIB_WRITE)UART6_Write,
+    .writeIsBusy = (DRV_USART_PLIB_WRITE_IS_BUSY)UART6_WriteIsBusy,
+    .writeCountGet = (DRV_USART_PLIB_WRITE_COUNT_GET)UART6_WriteCountGet,
+    .errorGet = (DRV_USART_PLIB_ERROR_GET)UART6_ErrorGet,
+    .serialSetup = (DRV_USART_PLIB_SERIAL_SETUP)UART6_SerialSetup
+};
+
+const uint32_t drvUsart0remapDataWidth[] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0x0, 0x6 };
+const uint32_t drvUsart0remapParity[] = { 0x0, 0x2, 0x4, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+const uint32_t drvUsart0remapStopBits[] = { 0x0, 0xFFFFFFFF, 0x1 };
+const uint32_t drvUsart0remapError[] = { 0x2, 0x8, 0x4 };
+
+const DRV_USART_INTERRUPT_SOURCES drvUSART0InterruptSources =
+{
+    /* Peripheral has more than one interrupt vector */
+    .isSingleIntSrc                        = false,
+
+    /* Peripheral interrupt lines */
+    .intSources.multi.usartTxCompleteInt   = _UART6_TX_VECTOR,
+    .intSources.multi.usartTxReadyInt      = -1,
+    .intSources.multi.usartRxCompleteInt   = _UART6_RX_VECTOR,
+    .intSources.multi.usartErrorInt        = _UART6_FAULT_VECTOR,
+};
+
+const DRV_USART_INIT drvUsart0InitData =
+{
+    .usartPlib = &drvUsart0PlibAPI,
+
+    /* USART Number of clients */
+    .numClients = DRV_USART_CLIENTS_NUMBER_IDX0,
+
+    /* USART Client Objects Pool */
+    .clientObjPool = (uintptr_t)&drvUSART0ClientObjPool[0],
+
+    .dmaChannelTransmit = SYS_DMA_CHANNEL_NONE,
+
+    .dmaChannelReceive = SYS_DMA_CHANNEL_NONE,
+
+    /* Combined size of transmit and receive buffer objects */
+    .bufferObjPoolSize = DRV_USART_QUEUE_SIZE_IDX0,
+
+    /* USART transmit and receive buffer buffer objects pool */
+    .bufferObjPool = (uintptr_t)&drvUSART0BufferObjPool[0],
+
+    .interruptSources = &drvUSART0InterruptSources,
+
+    .remapDataWidth = drvUsart0remapDataWidth,
+
+    .remapParity = drvUsart0remapParity,
+
+    .remapStopBits = drvUsart0remapStopBits,
+
+    .remapError = drvUsart0remapError,
+};
+
+// </editor-fold>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+    /* Start out with interrupts disabled before configuring any modules */
+    __builtin_disable_interrupts();
+
+  
+    CLK_Initialize();
+	GPIO_Initialize();
+
+    /* Configure CP0.K0 for optimal performance (cached instruction pre-fetch) */
+    __builtin_mtc0(16, 0,(__builtin_mfc0(16, 0) | 0x3));
+
+    /* Configure Wait States and Prefetch */
+    CHECONbits.PFMWS = 3;
+    CHECONbits.PREFEN = 1;
+
+
+
+	UART4_Initialize();
+
+	BSP_Initialize();
+	UART6_Initialize();
+
+
+    sysObj.drvUsart1 = DRV_USART_Initialize(DRV_USART_INDEX_1, (SYS_MODULE_INIT *)&drvUsart1InitData);
+
+    sysObj.drvUsart0 = DRV_USART_Initialize(DRV_USART_INDEX_0, (SYS_MODULE_INIT *)&drvUsart0InitData);
+
+
+
+
+    APP1_Initialize();
+    APP2_Initialize();
+
+
+    EVIC_Initialize();
+
+	/* Enable global interrupts */
+    __builtin_enable_interrupts();
+
+
+}
+
+
+/*******************************************************************************
+ End of File
+*/
