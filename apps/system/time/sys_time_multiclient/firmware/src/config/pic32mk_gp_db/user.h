8eb5aaf70423b7f5dc60a458d7cbcbee4fe0df44
Mohan RP <mohan.kumar@microchip.com>
2019-03-13 09:25:35 +0530

    [SYS_TIME] Created sys_time_multiclient application for pic32mk
    MH3-8787

---
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32mk_gp_db/user.h
@@ -0,0 +1,48 @@
+/*******************************************************************************
+  User Configuration Header
+
+  File Name:
+    user.h
+
+  Summary:
+    Build-time configuration header for the user defined by this project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    It only provides macro definitions for build-time configuration options
+
+*******************************************************************************/
+
+#ifndef USER_H
+#define USER_H
+#include "bsp/bsp.h"
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: User Configuration macros
+// *****************************************************************************
+// *****************************************************************************
+#define SWITCH_Get()                    SWITCH1_Get()
+#define SWITCH_STATE_PRESSED            SWITCH1_STATE_PRESSED
+#define LED_TOGGLE()                    LED1_Toggle()
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // USER_H
+/*******************************************************************************
+ End of File
+*/
