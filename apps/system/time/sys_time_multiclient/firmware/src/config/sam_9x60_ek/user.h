c86cd9711040ba114a134715beb0d1ea2f2c25d7
Syam Nair <syam.nair@microchip.com>
2019-04-08 19:55:40 +0530

    [SYS_TIME] Create sys_time_multiclient application for SAM9X60
    MH3-9382

---
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_9x60_ek/user.h
@@ -0,0 +1,51 @@
+/*******************************************************************************
+  User Configuration Header
+
+  File Name:
+    user.h
+
+  Summary:
+    Build-time configuration header for the user defined by this project.
+
+  Description:
+    An MPLAB Project may have multiple configurations.  This file defines the
+    build-time options for a single configuration.
+
+  Remarks:
+    It only provides macro definitions for build-time configuration options
+
+*******************************************************************************/
+
+#ifndef USER_H
+#define USER_H
+
+// DOM-IGNORE-BEGIN
+#ifdef __cplusplus  // Provide C++ Compatibility
+
+extern "C" {
+
+#endif
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: User Configuration macros
+// *****************************************************************************
+// *****************************************************************************
+#include "bsp/bsp.h"
+  
+#define SWITCH_Get SW1_Get
+#define SWITCH_STATE_PRESSED SW1_STATE_PRESSED
+#define LED_TOGGLE LED_BLUE_Toggle
+
+
+//DOM-IGNORE-BEGIN
+#ifdef __cplusplus
+}
+#endif
+//DOM-IGNORE-END
+
+#endif // USER_H
+/*******************************************************************************
+ End of File
+*/
