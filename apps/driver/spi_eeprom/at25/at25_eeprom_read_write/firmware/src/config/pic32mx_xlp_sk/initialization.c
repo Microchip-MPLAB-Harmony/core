47f36aaa9e0afc84faea235b325fa4f5cf714b72
Santosh Tangudu <santosh.tangudu@microchip.com>
2019-04-16 15:02:25 +0530

    [AT25] Cretaed and generated spi_eeprom\at25\at25_eeprom_read_write demo for pic32mx_xlp_sk board
    MH3-9549

---
+++ apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/pic32mx_xlp_sk/initialization.c
@@ -0,0 +1,242 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+/*** DEVCFG0 ***/
+#pragma config DEBUG =      OFF
+#pragma config JTAGEN =     OFF
+#pragma config ICESEL =     ICS_PGx2
+#pragma config PWP =        OFF
+#pragma config BWP =        OFF
+#pragma config CP =         OFF
+#pragma config SMCLR =      MCLR_NORM
+
+
+/*** DEVCFG1 ***/
+#pragma config FNOSC =      SPLL
+#pragma config FPBDIV =     DIV_2
+#pragma config FSOSCEN =    ON
+#pragma config IESO =       ON
+#pragma config POSCMOD =    OFF
+#pragma config OSCIOFNC =   OFF
+#pragma config FCKSM =      CSDCMD
+#pragma config WDTPS =      PS1048576
+#pragma config WDTSPGM =    ON
+#pragma config FWDTEN =     OFF
+#pragma config WINDIS =     OFF
+#pragma config FWDTWINSZ =  WINSZ_25
+
+
+/*** DEVCFG2 ***/
+#pragma config BOREN =    ON
+#pragma config FPLLIDIV =   DIV_2
+#pragma config FPLLICLK =   PLL_FRC
+#pragma config FPLLMUL =    MUL_18
+#pragma config FPLLODIV =   DIV_1
+#pragma config DSBOREN =    ON
+#pragma config DSWDTPS =    DSPS32
+#pragma config DSWDTOSC =   LPRC
+#pragma config DSWDTEN =    OFF
+#pragma config FDSEN =      ON
+#pragma config UPLLEN =     OFF
+#pragma config UPLLIDIV =   DIV_3
+
+/*** DEVCFG3 ***/
+#pragma config AI2C1 =      OFF
+#pragma config AI2C2 =      OFF
+#pragma config USERID =     0xffff
+#pragma config PMDL1WAY =   ON
+#pragma config IOL1WAY =    ON
+#pragma config FUSBIDIO =   ON
+
+
+
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="DRV_AT25 Initialization Data">
+
+/* SPI PLIB Interface Initialization for AT25 Driver */
+const DRV_AT25_PLIB_INTERFACE drvAT25PlibAPI = {
+
+    /* SPI PLIB WriteRead function */
+    .writeRead = (DRV_AT25_PLIB_WRITE_READ)SPI1_WriteRead,
+
+    /* SPI PLIB Write function */
+    .write = (DRV_AT25_PLIB_WRITE)SPI1_Write,
+
+    /* SPI PLIB Read function */
+    .read = (DRV_AT25_PLIB_READ)SPI1_Read,
+
+    /* SPI PLIB Transfer Status function */
+    .isBusy = (DRV_AT25_PLIB_IS_BUSY)SPI1_IsBusy,
+
+    /* SPI PLIB Callback Register */
+    .callbackRegister = (DRV_AT25_PLIB_CALLBACK_REGISTER)SPI1_CallbackRegister,
+};
+
+/* AT25 Driver Initialization Data */
+const DRV_AT25_INIT drvAT25InitData =
+{
+    /* SPI PLIB API  interface*/
+    .spiPlib = &drvAT25PlibAPI,
+
+    /* AT25 Number of clients */
+    .numClients = DRV_AT25_CLIENTS_NUMBER_IDX,
+
+    /* EEPROM Page Size in bytes */
+    .pageSize = DRV_AT25_EEPROM_PAGE_SIZE,
+
+    /* Total size of the EEPROM in bytes */
+    .flashSize = DRV_AT25_EEPROM_FLASH_SIZE,
+
+    .blockStartAddress =    0x0,
+
+    .chipSelectPin    = DRV_AT25_CHIP_SELECT_PIN_IDX,
+
+    .holdPin    = DRV_AT25_HOLD_PIN_IDX,
+
+    .writeProtectPin    = DRV_AT25_WP_PIN_IDX,
+};
+
+// </editor-fold>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+    /* Start out with interrupts disabled before configuring any modules */
+    __builtin_disable_interrupts();
+
+  
+    CLK_Initialize();
+	GPIO_Initialize();
+
+    /* Configure KSEG0 as cacheable memory. This is needed for Prefetch Buffer */
+    __builtin_mtc0(16, 0,(__builtin_mfc0(16, 0) | 0x3));
+
+    /* Configure Flash Wait States and Prefetch */
+    CHECONbits.PFMWS = 3;
+    CHECONbits.PREFEN = 3;
+
+    /* Set the SRAM wait states to zero */
+    BMXCONbits.BMXWSDRM = 0;
+
+
+
+
+	BSP_Initialize();
+	SPI1_Initialize();
+
+
+    sysObj.drvAT25 = DRV_AT25_Initialize(DRV_AT25_INDEX, (SYS_MODULE_INIT *)&drvAT25InitData);
+
+
+
+
+    APP_Initialize();
+
+
+    EVIC_Initialize();
+
+	/* Enable global interrupts */
+    __builtin_enable_interrupts();
+
+
+}
+
+
+/*******************************************************************************
+ End of File
+*/
