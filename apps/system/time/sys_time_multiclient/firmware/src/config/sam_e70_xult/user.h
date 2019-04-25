6ec7b95e3374cb472b46cf6619c0d3798ff16c0b
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-04-25 09:30:45 +0530

    [SYS_TIME] Updated the application to fix the SWITCH_STATUS_PRESSED macro build error
    MH3-9711

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_e70_xult/user.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_e70_xult/user.h
@@ -62,8 +62,8 @@ extern "C" {
 #define LED_OFF()                   LED1_Off()
 #define LED_TOGGLE()                LED1_Toggle()
 
-#define SWITCH_GET()            	SWITCH1_Get()
-#define SWITCH_STATUS_PRESSED   	SWITCH1_STATE_PRESSED
+#define SWITCH_GET()            	SWITCH_Get()
+#define SWITCH_STATUS_PRESSED   	SWITCH_STATE_PRESSED
 
 //DOM-IGNORE-BEGIN
 #ifdef __cplusplus
