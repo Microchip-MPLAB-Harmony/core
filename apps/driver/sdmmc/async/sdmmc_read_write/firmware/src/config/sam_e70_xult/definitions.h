c2ba3aa6a11168cea94a2d8ce39c6f45b21b5493
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-04-26 20:37:57 +0530

    [SDMMC] Regenerated sdmmc/async/sdmmc_read_write demo for sam_e70_xut configuration
    MH3-11259

--- apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_e70_xult/definitions.h
@@ -54,6 +54,7 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/xdmac/plib_xdmac.h"
+#include "peripheral/efc/plib_efc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "peripheral/hsmci/plib_hsmci.h"
 #include "system/int/sys_int.h"
