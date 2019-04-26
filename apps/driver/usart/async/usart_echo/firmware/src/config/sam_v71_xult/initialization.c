5630a7c10b989f8ffeed3c14f99206eadf586b4a
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-04-26 15:18:17 +0530

    [USART] Regenerated usart/async/usart_echo demo for sam_v71_xult application
    MH3-11473

--- apps/driver/usart/async/usart_echo/firmware/src/config/sam_v71_xult/initialization.c
+++ apps/driver/usart/async/usart_echo/firmware/src/config/sam_v71_xult/initialization.c
@@ -54,7 +54,6 @@
 // Section: Configuration Bits
 // ****************************************************************************
 // ****************************************************************************
-
 #pragma config TCM_CONFIGURATION = 0
 #pragma config SECURITY_BIT = CLEAR
 #pragma config BOOT_MODE = SET
@@ -170,10 +169,11 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
+
+    EFC_Initialize();
   
     CLK_Initialize();
 	PIO_Initialize();
-
 
 	RSWDT_REGS->RSWDT_MR = RSWDT_MR_WDDIS_Msk;	// Disable RSWDT 
 
