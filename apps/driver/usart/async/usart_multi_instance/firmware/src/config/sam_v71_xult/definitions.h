5866404941b7fd5d2b05dbc7fe3953711523a135
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-04-29 09:18:31 +0530

    [USART] Regenerated usart/async/usart_multi_instance demo for sam_v71_xult configuration
    MH3-11491

--- apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_v71_xult/definitions.h
@@ -52,6 +52,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/efc/plib_efc.h"
 #include "peripheral/usart/plib_usart1.h"
 #include "bsp/bsp.h"
 #include "peripheral/usart/plib_usart0.h"
