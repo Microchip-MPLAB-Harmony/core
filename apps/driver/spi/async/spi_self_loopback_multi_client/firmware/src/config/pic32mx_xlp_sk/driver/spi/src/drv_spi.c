cd43d8c663f93fc8b3a328623cc012acc8ba4151
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-04-30 22:09:13 +0530

    [SPI] Regenerated and tested spi_async_spi_self_loopback_multi_client_pic32mx_xlp_sk configuration
    MH3-11299
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32mx_xlp_sk/driver/spi/src/drv_spi.c
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/pic32mx_xlp_sk/driver/spi/src/drv_spi.c
@@ -652,6 +652,9 @@ void _DRV_SPI_RX_DMA_CallbackHandler(
     }
     else
     {
+        /* Make sure the shift register is empty before de-asserting the CS line */
+        while (dObj->spiPlib->isBusy());
+
         /* Transfer complete. De-assert Chip Select if it is defined by user. */
         if(clientObj->setup.chipSelect != SYS_PORT_PIN_NONE)
         {
