42c35b16b1e4feddecd52986184ac6b4c7e660cb
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-04-30 20:16:36 +0530

    [I2C] Regenerated i2c/async/i2c_eeprom demo for sam_e70_xult configuration
    MH3-11175

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_e70_xult/definitions.h
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_e70_xult/definitions.h
@@ -53,6 +53,7 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/twihs/plib_twihs0.h"
+#include "peripheral/efc/plib_efc.h"
 #include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
