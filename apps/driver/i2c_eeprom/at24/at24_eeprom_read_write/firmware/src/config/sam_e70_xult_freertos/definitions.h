bfa3981539a95273a9eb4afcd4de2955973c3262
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-05-01 18:18:29 +0530

    [I2C_EEPROM] Regenerated i2c_eeprom/at24/at24_eeprom_read_write demo for sam_e70_xult_freertos configuration
    MH3-11235

--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_e70_xult_freertos/definitions.h
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_e70_xult_freertos/definitions.h
@@ -54,6 +54,7 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/twihs/plib_twihs0.h"
+#include "peripheral/efc/plib_efc.h"
 #include "FreeRTOS.h"
 #include "task.h"
 #include "system/int/sys_int.h"
