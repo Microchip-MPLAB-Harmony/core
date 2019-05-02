985ebb50553fdc6d1f6e70f3e4fbf56fbabe75c0
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-05-02 18:46:32 +0530

    [FreeRTOS] Regenerated and tested basic_freertos_sam_v71_xult configuration
    MH3-11717
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/rtos/freertos/basic_freertos/firmware/src/config/sam_v71_xult/definitions.h
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/sam_v71_xult/definitions.h
@@ -51,6 +51,7 @@
 #include "peripheral/clk/plib_clk.h"
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/efc/plib_efc.h"
 #include "bsp/bsp.h"
 #include "FreeRTOS.h"
 #include "task.h"
