212965c69168c53d95f058a5a9cafff98bf50dcf
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-05-02 19:38:15 +0530

    [FreeRTOS] Regenerated and tested basic_freertos_sam_d20_xpro configuration
    MH3-11705
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/rtos/freertos/basic_freertos/firmware/src/config/sam_d20_xpro/definitions.h
+++ apps/rtos/freertos/basic_freertos/firmware/src/config/sam_d20_xpro/definitions.h
@@ -48,6 +48,7 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <stdbool.h>
+#include "peripheral/nvmctrl/plib_nvmctrl.h"
 #include "peripheral/evsys/plib_evsys.h"
 #include "bsp/bsp.h"
 #include "peripheral/port/plib_port.h"
