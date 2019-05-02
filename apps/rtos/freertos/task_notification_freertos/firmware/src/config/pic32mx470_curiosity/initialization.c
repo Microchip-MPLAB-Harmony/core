9830a4b9764b8325644c277522376214a5ef49ee
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-05-02 20:30:05 +0530

    [FreeRTOS] Regenerated and tested task_notification_freertos_pic32mx470_curiosity configuration
    MH3-11721
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/rtos/freertos/task_notification_freertos/firmware/src/config/pic32mx470_curiosity/initialization.c
+++ apps/rtos/freertos/task_notification_freertos/firmware/src/config/pic32mx470_curiosity/initialization.c
@@ -57,7 +57,7 @@
 
 /*** DEVCFG0 ***/
 #pragma config DEBUG =      OFF
-#pragma config JTAGEN =     ON
+#pragma config JTAGEN =     OFF
 #pragma config ICESEL =     ICS_PGx1
 #pragma config PWP =        OFF
 #pragma config BWP =        OFF
