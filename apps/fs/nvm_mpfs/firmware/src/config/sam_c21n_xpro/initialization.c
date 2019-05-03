e7a2889b56b9b374441ea87b8bc4e77b1100b74b
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-05-03 21:41:13 +0530

    [SYS_FS] Regenerated and tested fs_nvm_mpfs_sam_c21n_xpro
    MH3-11547
    
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/fs/nvm_mpfs/firmware/src/config/sam_c21n_xpro/initialization.c
+++ apps/fs/nvm_mpfs/firmware/src/config/sam_c21n_xpro/initialization.c
@@ -54,7 +54,6 @@
 // Section: Configuration Bits
 // ****************************************************************************
 // ****************************************************************************
-
 #pragma config NVMCTRL_BOOTPROT = SIZE_0BYTES
 #pragma config NVMCTRL_EEPROM_SIZE = SIZE_0BYTES
 #pragma config BODVDDUSERLEVEL = 0x8 // Enter Hexadecimal value
@@ -71,6 +70,7 @@
 #pragma config WDT_WINDOW = CYC8
 #pragma config WDT_EWOFFSET = CYC8
 #pragma config WDT_WEN = DISABLED
+
 
 
 
@@ -171,14 +171,13 @@ const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 
 void SYS_Initialize ( void* data )
 {
+    NVMCTRL_Initialize( );
+
   
     PORT_Initialize();
 
     CLOCK_Initialize();
 
-
-
-    NVMCTRL_Initialize( );
 
 	BSP_Initialize();
 
