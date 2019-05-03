aa6c709a04bfb428bcb6893df4c713f6082410ed
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2019-05-03 10:50:13 +0530

    [SPI] Regenerated and tested spi_multi_slave sync demo for sama5d2 DFT
    MH3-11879

--- apps/driver/spi/sync/spi_multi_slave/firmware/src/config/sam_a5d2_xult_freertos/interrupts.c
+++ apps/driver/spi/sync/spi_multi_slave/firmware/src/config/sam_a5d2_xult_freertos/interrupts.c
@@ -163,9 +163,9 @@ void SYSC_SharedHandler( void )
 
 /* data for irq register initialization */
 IrqData irqData[] = {
-    { 3,   (uint32_t) AIC_REGS,    FreeRTOS_Tick_Handler,      AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
-    { 6,   (uint32_t) AIC_REGS,    XDMAC0_InterruptHandler,    AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
-    { 34,  (uint32_t) AIC_REGS,    SPI1_InterruptHandler,      AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
+    { 3,   (uint32_t) AIC_REGS,    FreeRTOS_Tick_Handler,      AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
+    { 6,   (uint32_t) AIC_REGS,    XDMAC0_InterruptHandler,    AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
+    { 34,  (uint32_t) AIC_REGS,    SPI1_InterruptHandler,      AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
 };
 
 uint32_t irqDataEntryCount = sizeof( irqData ) / sizeof( irqData[ 0 ]);
