8a4f91dd9a5243201dc9b1790159f34c672c389b
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2019-05-03 11:00:31 +0530

    [SPI] Regenerated and tested spi_multi_slave async demo for sama5d2 DFT
    MH3-11865

--- apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_a5d2_xult/interrupts.c
+++ apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_a5d2_xult/interrupts.c
@@ -162,8 +162,8 @@ void SYSC_SharedHandler( void )
 
 /* data for irq register initialization */
 IrqData irqData[] = {
-    { 3,   (uint32_t) AIC_REGS,    PIT_InterruptHandler,       AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
-    { 34,  (uint32_t) AIC_REGS,    SPI1_InterruptHandler,      AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  AIC_SMR_PRIORITY_MINIMUM_Val   },
+    { 3,   (uint32_t) AIC_REGS,    PIT_InterruptHandler,       AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
+    { 34,  (uint32_t) AIC_REGS,    SPI1_InterruptHandler,      AIC_SMR_SRCTYPE_INT_LEVEL_SENSITIVE_Val,  0x0 },
 };
 
 uint32_t irqDataEntryCount = sizeof( irqData ) / sizeof( irqData[ 0 ]);
