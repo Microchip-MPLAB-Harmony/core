6ed42c6a59a3b271c4d2d8650cfe8397227a87cc
Amit Raddi <amit.raddi@microchip.com>
2019-06-17 17:25:46 +0530

    [SDMMC] Regenerated demo for driver_sdmmc_async_sdmmc_read_write_sam_v71_xult
    MH3-15423
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_v71_xult/peripheral/pio/plib_pio.c
+++ apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_v71_xult/peripheral/pio/plib_pio.c
@@ -96,6 +96,8 @@ void PIO_Initialize ( void )
     /* PORTB Output Direction Enable */
     ((pio_registers_t*)PIO_PORT_B)->PIO_OER = 0x0;
     ((pio_registers_t*)PIO_PORT_B)->PIO_ODR = ~0x0;
+    /* PORTB Initial state High */
+    ((pio_registers_t*)PIO_PORT_B)->PIO_ODSR = 0x0;
     /* PORTB drive control */
     ((pio_registers_t*)PIO_PORT_B)->PIO_DRIVER = 0x0;
 
@@ -128,6 +130,8 @@ void PIO_Initialize ( void )
     /* PORTD Output Direction Enable */
     ((pio_registers_t*)PIO_PORT_D)->PIO_OER = 0x0;
     ((pio_registers_t*)PIO_PORT_D)->PIO_ODR = ~0x0;
+    /* PORTD Initial state High */
+    ((pio_registers_t*)PIO_PORT_D)->PIO_ODSR = 0x0;
     /* PORTD drive control */
     ((pio_registers_t*)PIO_PORT_D)->PIO_DRIVER = 0x0;
 
@@ -143,6 +147,8 @@ void PIO_Initialize ( void )
     /* PORTE Output Direction Enable */
     ((pio_registers_t*)PIO_PORT_E)->PIO_OER = 0x0;
     ((pio_registers_t*)PIO_PORT_E)->PIO_ODR = ~0x0;
+    /* PORTE Initial state High */
+    ((pio_registers_t*)PIO_PORT_E)->PIO_ODSR = 0x0;
     /* PORTE drive control */
     ((pio_registers_t*)PIO_PORT_E)->PIO_DRIVER = 0x0;
 
