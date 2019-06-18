39c030eabee181a668d6d70efe9d0dffef62e996
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-06-18 10:03:59 +0530

    [I2C] Regenerated demo for driver_i2c_async_i2c_eeprom_sam_g55_xpro
    MH3-15309
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_g55_xpro/peripheral/pio/plib_pio.c
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_g55_xpro/peripheral/pio/plib_pio.c
@@ -62,7 +62,8 @@ void PIO_Initialize ( void )
     ((pio_registers_t*)PIO_PORT_A)->PIO_PER = 0xFFFFFFFF;
     ((pio_registers_t*)PIO_PORT_A)->PIO_MDDR = 0xFFFFFFFF;
     /* PORTA Pull Up Enable/Disable as per MHC selection */
-    ((pio_registers_t*)PIO_PORT_A)->PIO_PUDR = 0xFFFFFFFF;
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PUDR = ~0x4;
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PUER = 0x4;
     /* PORTA Pull Down Enable/Disable as per MHC selection */
     ((pio_registers_t*)PIO_PORT_A)->PIO_PPDDR = 0xFFFFFFFF;
     /* PORTA Output Write Enable */
