74109d6f3762c6ee3602648ee72f14b42814d11e
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-06-18 10:49:02 +0530

    [I2C] Regenerated demo for driver_i2c_async_i2c_multi_slave_sam_g55_xpro
    MH3-15331
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_g55_xpro/peripheral/pio/plib_pio.c
+++ apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_g55_xpro/peripheral/pio/plib_pio.c
@@ -67,7 +67,8 @@ void PIO_Initialize ( void )
     ((pio_registers_t*)PIO_PORT_A)->PIO_PER = ~0x18000000;
     ((pio_registers_t*)PIO_PORT_A)->PIO_MDDR = 0xFFFFFFFF;
     /* PORTA Pull Up Enable/Disable as per MHC selection */
-    ((pio_registers_t*)PIO_PORT_A)->PIO_PUDR = 0xFFFFFFFF;
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PUDR = ~0x4;
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PUER = 0x4;
     /* PORTA Pull Down Enable/Disable as per MHC selection */
     ((pio_registers_t*)PIO_PORT_A)->PIO_PPDDR = 0xFFFFFFFF;
     /* PORTA Output Write Enable */
@@ -89,8 +90,7 @@ void PIO_Initialize ( void )
     ((pio_registers_t*)PIO_PORT_B)->PIO_PER = ~0xc00;
     ((pio_registers_t*)PIO_PORT_B)->PIO_MDDR = 0xFFFFFFFF;
     /* PORTB Pull Up Enable/Disable as per MHC selection */
-    ((pio_registers_t*)PIO_PORT_B)->PIO_PUDR = ~0xc00;
-    ((pio_registers_t*)PIO_PORT_B)->PIO_PUER = 0xc00;
+    ((pio_registers_t*)PIO_PORT_B)->PIO_PUDR = 0xFFFFFFFF;
     /* PORTB Pull Down Enable/Disable as per MHC selection */
     ((pio_registers_t*)PIO_PORT_B)->PIO_PPDDR = 0xFFFFFFFF;
     /* PORTB Output Write Enable */
