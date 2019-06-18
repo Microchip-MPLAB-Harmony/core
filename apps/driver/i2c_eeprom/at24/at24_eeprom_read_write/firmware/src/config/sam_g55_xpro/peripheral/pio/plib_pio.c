67ff6072480c22841989c6b852342909595321fd
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-06-18 10:21:55 +0530

    [I2C_EEPROM] Regenerated demo for driver_i2c_eeprom_at24_at24_eeprom_read_write_sam_g55_xpro
    MH3-15385
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_g55_xpro/peripheral/pio/plib_pio.c
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/sam_g55_xpro/peripheral/pio/plib_pio.c
@@ -62,7 +62,8 @@ void PIO_Initialize ( void )
     ((pio_registers_t*)PIO_PORT_A)->PIO_PER = 0xFFFFFFFF;
     ((pio_registers_t*)PIO_PORT_A)->PIO_MDDR = 0xFFFFFFFF;
     /* PORTA Pull Up Enable/Disable as per MHC selection */
-    ((pio_registers_t*)PIO_PORT_A)->PIO_PUDR = 0xFFFFFFFF;
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PUDR = ~0x4;
+    ((pio_registers_t*)PIO_PORT_A)->PIO_PUER = 0x4;
     /* PORTA Pull Down Enable/Disable as per MHC selection */
     ((pio_registers_t*)PIO_PORT_A)->PIO_PPDDR = 0xFFFFFFFF;
     /* PORTA Output Write Enable */
@@ -84,8 +85,7 @@ void PIO_Initialize ( void )
     ((pio_registers_t*)PIO_PORT_B)->PIO_PER = ~0xc00;
     ((pio_registers_t*)PIO_PORT_B)->PIO_MDDR = 0xFFFFFFFF;
     /* PORTB Pull Up Enable/Disable as per MHC selection */
-    ((pio_registers_t*)PIO_PORT_B)->PIO_PUDR = ~0xc00;
-    ((pio_registers_t*)PIO_PORT_B)->PIO_PUER = 0xc00;
+    ((pio_registers_t*)PIO_PORT_B)->PIO_PUDR = 0xFFFFFFFF;
     /* PORTB Pull Down Enable/Disable as per MHC selection */
     ((pio_registers_t*)PIO_PORT_B)->PIO_PPDDR = 0xFFFFFFFF;
     /* PORTB Output Write Enable */
