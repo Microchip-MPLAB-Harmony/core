620724cf096c29118b54b270fb7c040487545d04
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-07-03 16:40:36 +0530

    [I2C_EEPROM] Added at24_eeprom_read_write application for pic32mk_curiosity_ultra board
    MH3-17218

---
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/pic32mk_curiosity_ultra/driver/at24/src/drv_at24_local.h
@@ -0,0 +1,154 @@
+/*******************************************************************************
+  AT24 Driver Local Data Structures
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    drv_at24_local.h
+
+  Summary:
+    AT24 Driver Local Data Structures
+
+  Description:
+    Driver Local Data Structures
+*******************************************************************************/
+
+//DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+//DOM-IGNORE-END
+
+#ifndef _DRV_AT24_LOCAL_H
+#define _DRV_AT24_LOCAL_H
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Data Type Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* DRV_AT24 Active Command
+
+  Summary:
+    Enumeration listing of the active command.
+
+  Description:
+    This enumeration defines the currently active command
+
+  Remarks:
+    None
+*/
+
+typedef enum
+{
+    /* Write command*/
+    DRV_AT24_CMD_WRITE,
+
+    /* Wait for EEPROM internal write complete command */
+    DRV_AT24_CMD_WAIT_WRITE_COMPLETE,
+
+    /* Read command */
+    DRV_AT24_CMD_READ,
+
+} DRV_AT24_CMD;
+
+// *****************************************************************************
+/* AT24 Driver Instance Object
+
+  Summary:
+    Object used to keep any data required for an instance of the AT24C driver.
+
+  Description:
+    None.
+
+  Remarks:
+    None.
+*/
+
+typedef struct
+{
+    /* Flag to indicate this object is in use  */
+    bool                            inUse;
+
+    /* Indicates th number of clients that have opened this driver */
+    size_t                          nClients;
+
+    /* Maximum number of clients */
+    size_t                          nClientsMax;
+
+    /* The status of the driver */
+    SYS_STATUS                      status;
+
+    /* PLIB API list that will be used by the driver to access the hardware */
+    const DRV_AT24_PLIB_INTERFACE*  i2cPlib;
+
+    /* EEPROM Slave Address*/
+    uint16_t                        slaveAddress;
+
+    /* Points to the next EEPROM memory address to write to */
+    uint32_t                        nextMemoryAddr;
+
+    /* Pointer to the write buffer to write data from */
+    uint8_t*                        nextBufferAddr;
+
+    /* Pointer to the write buffer to write data from */
+    uint32_t                        nPendingBytes;
+
+    /* Page size information */
+    uint32_t                        pageSize;
+
+    /* Total flash size */
+    uint32_t                        flashSize;
+
+    /* Starting memory address of EEPROM */
+    uint32_t                        blockStartAddress;
+
+    /* Write buffer - must also hold the EEPROM memory address */
+    uint8_t                         writeBuffer[DRV_AT24_WRITE_BUFFER_SIZE];
+
+    /* The command currently being executed */
+    DRV_AT24_CMD                    command;
+
+    /* Application event handler */
+    DRV_AT24_EVENT_HANDLER          eventHandler;
+
+    /* Application context */
+    uintptr_t                       context;
+
+    /* Status of the transfer */
+    volatile DRV_AT24_TRANSFER_STATUS       transferStatus;
+
+} DRV_AT24_OBJ;
+
+
+#endif //#ifndef _DRV_AT24C_LOCAL_H
---
+++ apps/driver/i2c_eeprom/at24/at24_eeprom_read_write/firmware/src/config/pic32mk_curiosity_ultra/initialization.c
@@ -0,0 +1,238 @@
+/*******************************************************************************
+  System Initialization File
+
+  File Name:
+    initialization.c
+
+  Summary:
+    This file contains source code necessary to initialize the system.
+
+  Description:
+    This file contains source code necessary to initialize the system.  It
+    implements the "SYS_Initialize" function, defines the configuration bits,
+    and allocates any necessary global system resources,
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+ *******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+#include "configuration.h"
+#include "definitions.h"
+#include "device.h"
+
+
+
+// ****************************************************************************
+// ****************************************************************************
+// Section: Configuration Bits
+// ****************************************************************************
+// ****************************************************************************
+
+/*** DEVCFG0 ***/
+#pragma config DEBUG =      OFF
+#pragma config JTAGEN =     OFF
+#pragma config ICESEL =     ICS_PGx2
+#pragma config TRCEN =      OFF
+#pragma config BOOTISA =    MIPS32
+#pragma config FSLEEP =     OFF
+#pragma config DBGPER =     PG_ALL
+#pragma config SMCLR =      MCLR_NORM
+#pragma config SOSCGAIN =   G2
+#pragma config SOSCBOOST =  ON
+#pragma config POSCGAIN =   G3
+#pragma config POSCBOOST =  ON
+#pragma config EJTAGBEN =   NORMAL
+
+/*** DEVCFG1 ***/
+#pragma config FNOSC =      SPLL
+#pragma config DMTINTV =    WIN_127_128
+#pragma config FSOSCEN =    ON
+#pragma config IESO =       ON
+#pragma config POSCMOD =    OFF
+#pragma config OSCIOFNC =   OFF
+#pragma config FCKSM =      CSECME
+#pragma config WDTPS =      PS1048576
+#pragma config WDTSPGM =    STOP
+#pragma config FWDTEN =     OFF
+#pragma config WINDIS =     NORMAL
+#pragma config FWDTWINSZ =  WINSZ_25
+#pragma config DMTCNT =     DMT31
+#pragma config FDMTEN =     OFF
+
+/*** DEVCFG2 ***/
+#pragma config FPLLIDIV =   DIV_1
+#pragma config FPLLRNG =    RANGE_5_10_MHZ
+#pragma config FPLLICLK =   PLL_FRC
+#pragma config FPLLMULT =   MUL_60
+#pragma config FPLLODIV =   DIV_4
+#pragma config BORSEL =     LOW
+
+/*** DEVCFG3 ***/
+#pragma config USERID =     0xffff
+#pragma config PGL1WAY =    ON
+#pragma config PMDL1WAY =   ON
+#pragma config IOL1WAY =    ON
+#pragma config PWMLOCK =  OFF
+
+/*** BF1SEQ ***/
+#pragma config TSEQ =       0x0
+#pragma config CSEQ =       0xffff
+
+
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Driver Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+// <editor-fold defaultstate="collapsed" desc="DRV_AT24 Initialization Data">
+
+/* I2C PLIB Interface Initialization for AT24 Driver */
+const DRV_AT24_PLIB_INTERFACE drvAT24PlibAPI = {
+
+    /* I2C PLIB WriteRead function */
+    .writeRead = (DRV_AT24_PLIB_WRITE_READ)I2C2_WriteRead,
+
+    /* I2C PLIB Write function */
+    .write = (DRV_AT24_PLIB_WRITE)I2C2_Write,
+
+    /* I2C PLIB Read function */
+    .read = (DRV_AT24_PLIB_READ)I2C2_Read,
+
+    /* I2C PLIB Transfer Status function */
+    .isBusy = (DRV_AT24_PLIB_IS_BUSY)I2C2_IsBusy,
+
+    /* I2C PLIB Error Status function */
+    .errorGet = (DRV_AT24_PLIB_ERROR_GET)I2C2_ErrorGet,
+
+    /* I2C PLIB Callback Register */
+    .callbackRegister = (DRV_AT24_PLIB_CALLBACK_REGISTER)I2C2_CallbackRegister,
+};
+
+/* AT24 Driver Initialization Data */
+const DRV_AT24_INIT drvAT24InitData =
+{
+    /* I2C PLIB API  interface*/
+    .i2cPlib = &drvAT24PlibAPI,
+
+    /* 7-bit I2C Slave address */
+    .slaveAddress = 0x54,
+
+    /* EEPROM Page Size in bytes */
+    .pageSize = DRV_AT24_EEPROM_PAGE_SIZE,
+
+    /* Total size of the EEPROM in bytes */
+    .flashSize = DRV_AT24_EEPROM_FLASH_SIZE,
+
+    /* AT24 Number of clients */
+    .numClients = DRV_AT24_CLIENTS_NUMBER_IDX,
+
+    .blockStartAddress =    0x0,
+};
+
+// </editor-fold>
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Data
+// *****************************************************************************
+// *****************************************************************************
+/* Structure to hold the object handles for the modules in the system. */
+SYSTEM_OBJECTS sysObj;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Library/Stack Initialization Data
+// *****************************************************************************
+// *****************************************************************************
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: System Initialization
+// *****************************************************************************
+// *****************************************************************************
+
+
+
+/*******************************************************************************
+  Function:
+    void SYS_Initialize ( void *data )
+
+  Summary:
+    Initializes the board, services, drivers, application and other modules.
+
+  Remarks:
+ */
+
+void SYS_Initialize ( void* data )
+{
+    /* Start out with interrupts disabled before configuring any modules */
+    __builtin_disable_interrupts();
+
+  
+    CLK_Initialize();
+	GPIO_Initialize();
+
+    /* Configure CP0.K0 for optimal performance (cached instruction pre-fetch) */
+    __builtin_mtc0(16, 0,(__builtin_mfc0(16, 0) | 0x3));
+
+    /* Configure Wait States and Prefetch */
+    CHECONbits.PFMWS = 3;
+    CHECONbits.PREFEN = 1;
+
+
+    I2C2_Initialize();
+
+	BSP_Initialize();
+
+    sysObj.drvAT24 = DRV_AT24_Initialize(DRV_AT24_INDEX, (SYS_MODULE_INIT *)&drvAT24InitData);
+
+
+
+
+    APP_Initialize();
+
+
+    EVIC_Initialize();
+
+	/* Enable global interrupts */
+    __builtin_enable_interrupts();
+
+
+}
+
+
+/*******************************************************************************
+ End of File
+*/
