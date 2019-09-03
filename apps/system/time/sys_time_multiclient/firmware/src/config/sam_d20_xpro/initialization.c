bde0f6c0b7a2cb20400decb17cf3f170772a497a
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-09-03 15:15:14 +0530

    [TIME] Regenerated demo for system_time_sys_time_multiclient_sam_d20_xpro MH3-19727
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_d20_xpro/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_d20_xpro/initialization.c
@@ -136,7 +136,7 @@ const SYS_TIME_INIT sysTimeInitData =
 
 void SYS_Initialize ( void* data )
 {
-    NVMCTRL_Initialize( );
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
   
     PORT_Initialize();
@@ -145,6 +145,8 @@ void SYS_Initialize ( void* data )
 
 
     SERCOM3_USART_Initialize();
+
+    NVMCTRL_Initialize( );
 
     EVSYS_Initialize();
 
