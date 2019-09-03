a8ffb83722de6724ed1dd0841d80708a888be9ac
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-09-03 18:06:50 +0530

    [USART] Regenerated demo for driver_usart_sync_usart_multi_instance_sam_v71_xult_freertos
    MH3-19397
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/sync/usart_multi_instance/firmware/src/config/sam_v71_xult_freertos/tasks.c
+++ apps/driver/usart/sync/usart_multi_instance/firmware/src/config/sam_v71_xult_freertos/tasks.c
@@ -53,6 +53,7 @@
 #include "configuration.h"
 #include "definitions.h"
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: RTOS "Tasks" Routine
@@ -94,7 +95,6 @@ void _APP_USART_USB_DEBUG_PORT_Tasks(  void *pvParameters  )
   Remarks:
     See prototype in system/common/sys_module.h.
 */
-
 void SYS_Tasks ( void )
 {
     /* Maintain system services */
@@ -133,7 +133,6 @@ void SYS_Tasks ( void )
     vTaskStartScheduler(); /* This function never returns. */
 
 }
-
 
 /*******************************************************************************
  End of File
