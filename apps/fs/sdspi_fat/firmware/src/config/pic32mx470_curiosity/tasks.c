eea64bf44444a75162fcde545473f7414951e918
Amit Raddi <amit.raddi@microchip.com>
2019-09-03 12:50:22 +0530

    [FS] Regenerated demo for fs_sdspi_fat_pic32mx470_curiosity
    MH3-19582
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/fs/sdspi_fat/firmware/src/config/pic32mx470_curiosity/tasks.c
+++ apps/fs/sdspi_fat/firmware/src/config/pic32mx470_curiosity/tasks.c
@@ -54,6 +54,7 @@
 #include "definitions.h"
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System "Tasks" Routine
@@ -67,7 +68,6 @@
   Remarks:
     See prototype in system/common/sys_module.h.
 */
-
 void SYS_Tasks ( void )
 {
     /* Maintain system services */
@@ -91,7 +91,6 @@ SYS_FS_Tasks();
 
 
 }
-
 
 /*******************************************************************************
  End of File
