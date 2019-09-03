80aa0f8529f685543556d8d8a36236c94b2d5ea7
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-09-03 17:16:31 +0530

    [USART] Regenerated demo for driver_usart_async_usart_multi_instance_sam_e70_xult
    MH3-19357
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_e70_xult/peripheral/usart/plib_usart0.c
+++ apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_e70_xult/peripheral/usart/plib_usart0.c
@@ -53,7 +53,7 @@ void static USART0_ISR_RX_Handler( void )
 {
     if(usart0Obj.rxBusyStatus == true)
     {
-        while((US_CSR_RXRDY_Msk == (USART0_REGS->US_CSR& US_CSR_RXRDY_Msk)) && (usart0Obj.rxSize > usart0Obj.rxProcessedSize) )
+        while((US_CSR_USART_RXRDY_Msk == (USART0_REGS->US_CSR& US_CSR_USART_RXRDY_Msk)) && (usart0Obj.rxSize > usart0Obj.rxProcessedSize) )
         {
             usart0Obj.rxBuffer[usart0Obj.rxProcessedSize++] = (USART0_REGS->US_RHR& US_RHR_RXCHR_Msk);
         }
@@ -65,7 +65,7 @@ void static USART0_ISR_RX_Handler( void )
             usart0Obj.rxBusyStatus = false;
 
             /* Disable Read, Overrun, Parity and Framing error interrupts */
-            USART0_REGS->US_IDR = (US_IDR_RXRDY_Msk | US_IDR_USART_LIN_FRAME_Msk | US_IDR_USART_LIN_PARE_Msk | US_IDR_OVRE_Msk);
+            USART0_REGS->US_IDR = (US_IDR_USART_RXRDY_Msk | US_IDR_USART_FRAME_Msk | US_IDR_USART_PARE_Msk | US_IDR_USART_OVRE_Msk);
 
             if(usart0Obj.rxCallback != NULL)
             {
@@ -86,7 +86,7 @@ void static USART0_ISR_TX_Handler( void )
 {
     if(usart0Obj.txBusyStatus == true)
     {
-        while((US_CSR_TXEMPTY_Msk == (USART0_REGS->US_CSR& US_CSR_TXEMPTY_Msk)) && (usart0Obj.txSize > usart0Obj.txProcessedSize) )
+        while((US_CSR_USART_TXEMPTY_Msk == (USART0_REGS->US_CSR& US_CSR_USART_TXEMPTY_Msk)) && (usart0Obj.txSize > usart0Obj.txProcessedSize) )
         {
             USART0_REGS->US_THR|= usart0Obj.txBuffer[usart0Obj.txProcessedSize++];
         }
@@ -95,7 +95,7 @@ void static USART0_ISR_TX_Handler( void )
         if(usart0Obj.txProcessedSize >= usart0Obj.txSize)
         {
             usart0Obj.txBusyStatus = false;
-            USART0_REGS->US_IDR = US_IDR_TXEMPTY_Msk;
+            USART0_REGS->US_IDR = US_IDR_USART_TXEMPTY_Msk;
 
             if(usart0Obj.txCallback != NULL)
             {
@@ -115,14 +115,14 @@ void static USART0_ISR_TX_Handler( void )
 void USART0_InterruptHandler( void )
 {
     /* Error status */
-    uint32_t errorStatus = (USART0_REGS->US_CSR & (US_CSR_OVRE_Msk | US_CSR_USART_LIN_FRAME_Msk | US_CSR_USART_LIN_PARE_Msk));
+    uint32_t errorStatus = (USART0_REGS->US_CSR & (US_CSR_USART_OVRE_Msk | US_CSR_USART_FRAME_Msk | US_CSR_USART_PARE_Msk));
 
     if(errorStatus != 0)
     {
         /* Client must call USARTx_ErrorGet() function to clear the errors */
 
         /* Disable Read, Overrun, Parity and Framing error interrupts */
-        USART0_REGS->US_IDR = (US_IDR_RXRDY_Msk | US_IDR_USART_LIN_FRAME_Msk | US_IDR_USART_LIN_PARE_Msk | US_IDR_OVRE_Msk);
+        USART0_REGS->US_IDR = (US_IDR_USART_RXRDY_Msk | US_IDR_USART_FRAME_Msk | US_IDR_USART_PARE_Msk | US_IDR_USART_OVRE_Msk);
 
         usart0Obj.rxBusyStatus = false;
 
@@ -135,13 +135,13 @@ void USART0_InterruptHandler( void )
     }
 
     /* Receiver status */
-    if(US_CSR_RXRDY_Msk == (USART0_REGS->US_CSR & US_CSR_RXRDY_Msk))
+    if(US_CSR_USART_RXRDY_Msk == (USART0_REGS->US_CSR & US_CSR_USART_RXRDY_Msk))
     {
         USART0_ISR_RX_Handler();
     }
 
     /* Transmitter status */
-    if(US_CSR_TXRDY_Msk == (USART0_REGS->US_CSR & US_CSR_TXRDY_Msk))
+    if(US_CSR_USART_TXRDY_Msk == (USART0_REGS->US_CSR & US_CSR_USART_TXRDY_Msk))
     {
         USART0_ISR_TX_Handler();
     }
@@ -154,10 +154,10 @@ void static USART0_ErrorClear( void )
 {
     uint8_t dummyData = 0u;
 
-    USART0_REGS->US_CR|= US_CR_RSTSTA_Msk;
+    USART0_REGS->US_CR |= US_CR_USART_RSTSTA_Msk;
 
     /* Flush existing error bytes from the RX FIFO */
-    while( US_CSR_RXRDY_Msk == (USART0_REGS->US_CSR& US_CSR_RXRDY_Msk) )
+    while( US_CSR_USART_RXRDY_Msk == (USART0_REGS->US_CSR& US_CSR_USART_RXRDY_Msk) )
     {
         dummyData = (USART0_REGS->US_RHR& US_RHR_RXCHR_Msk);
     }
@@ -171,13 +171,13 @@ void static USART0_ErrorClear( void )
 void USART0_Initialize( void )
 {
     /* Reset USART0 */
-    USART0_REGS->US_CR = (US_CR_RSTRX_Msk | US_CR_RSTTX_Msk | US_CR_RSTSTA_Msk);
+    USART0_REGS->US_CR = (US_CR_USART_RSTRX_Msk | US_CR_USART_RSTTX_Msk | US_CR_USART_RSTSTA_Msk);
 
     /* Enable USART0 */
-    USART0_REGS->US_CR = (US_CR_TXEN_Msk | US_CR_RXEN_Msk);
+    USART0_REGS->US_CR = (US_CR_USART_TXEN_Msk | US_CR_USART_RXEN_Msk);
 
     /* Configure USART0 mode */
-    USART0_REGS->US_MR = (US_MR_USCLKS_MCK | US_MR_CHRL_8_BIT | US_MR_USART_PAR_NO | US_MR_USART_NBSTOP_1_BIT | (0 << US_MR_USART_OVER_Pos));
+    USART0_REGS->US_MR = (US_MR_USART_USCLKS_MCK | US_MR_USART_CHRL_8_BIT | US_MR_USART_PAR_NO | US_MR_USART_NBSTOP_1_BIT | (0 << US_MR_USART_OVER_Pos));
 
     /* Configure USART0 Baud Rate */
     USART0_REGS->US_BRGR = US_BRGR_CD(81);
@@ -202,7 +202,7 @@ USART_ERROR USART0_ErrorGet( void )
     USART_ERROR errors = USART_ERROR_NONE;
     uint32_t status = USART0_REGS->US_CSR;
 
-    errors = (USART_ERROR)(status & (US_CSR_OVRE_Msk | US_CSR_USART_LIN_PARE_Msk | US_CSR_USART_LIN_FRAME_Msk));
+    errors = (USART_ERROR)(status & (US_CSR_USART_OVRE_Msk | US_CSR_USART_PARE_Msk | US_CSR_USART_FRAME_Msk));
 
     if(errors != USART_ERROR_NONE)
     {
@@ -240,16 +240,25 @@ bool USART0_SerialSetup( USART_SERIAL_SETUP *setup, uint32_t srcClkFreq )
         {
             brgVal = (srcClkFreq / (16 * baud));
         }
-        else
+        else if (srcClkFreq >= (8 * baud))
         {
             brgVal = (srcClkFreq / (8 * baud));
             overSampVal = US_MR_USART_OVER(1);
         }
+        else
+        {
+            return false;
+        }
+        if (brgVal > 65535)
+        {
+            /* The requested baud is so low that the ratio of srcClkFreq to baud exceeds the 16-bit register value of CD register */
+            return false;
+        }
 
         /* Configure USART0 mode */
         usartMode = USART0_REGS->US_MR;
-        usartMode &= ~(US_MR_CHRL_Msk | US_MR_USART_MODE9_Msk | US_MR_USART_PAR_Msk | US_MR_USART_NBSTOP_Msk | US_MR_USART_OVER_Msk);
-        USART0_REGS->US_MR = usartMode | (setup->dataWidth | setup->parity | setup->stopBits | overSampVal);
+        usartMode &= ~(US_MR_USART_CHRL_Msk | US_MR_USART_MODE9_Msk | US_MR_USART_PAR_Msk | US_MR_USART_NBSTOP_Msk | US_MR_USART_OVER_Msk);
+        USART0_REGS->US_MR = usartMode | ((uint32_t)setup->dataWidth | (uint32_t)setup->parity | (uint32_t)setup->stopBits | (uint32_t)overSampVal);
 
         /* Configure USART0 Baud Rate */
         USART0_REGS->US_BRGR = US_BRGR_CD(brgVal);
@@ -280,7 +289,7 @@ bool USART0_Read( void *buffer, const size_t size )
             status = true;
 
             /* Enable Read, Overrun, Parity and Framing error interrupts */
-            USART0_REGS->US_IER = (US_IER_RXRDY_Msk | US_IER_USART_LIN_FRAME_Msk | US_IER_USART_LIN_PARE_Msk | US_IER_OVRE_Msk);
+            USART0_REGS->US_IER = (US_IER_USART_RXRDY_Msk | US_IER_USART_FRAME_Msk | US_IER_USART_PARE_Msk | US_IER_USART_OVRE_Msk);
         }
     }
 
@@ -304,13 +313,13 @@ bool USART0_Write( void *buffer, const size_t size )
             status = true;
 
             /* Initiate the transfer by sending first byte */
-            if(US_CSR_TXRDY_Msk == (USART0_REGS->US_CSR & US_CSR_TXRDY_Msk))
+            if(US_CSR_USART_TXRDY_Msk == (USART0_REGS->US_CSR & US_CSR_USART_TXRDY_Msk))
             {
                 USART0_REGS->US_THR = (US_THR_TXCHR(*lBuffer) & US_THR_TXCHR_Msk);
                 usart0Obj.txProcessedSize++;
             }
 
-            USART0_REGS->US_IER = US_IER_TXEMPTY_Msk;
+            USART0_REGS->US_IER = US_IER_USART_TXEMPTY_Msk;
         }
     }
 
