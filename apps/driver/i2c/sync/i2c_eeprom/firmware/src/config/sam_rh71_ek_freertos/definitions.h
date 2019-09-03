78119c433ec376a7bc6b503a080f22e9394cfca0
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-09-03 15:25:57 +0530

    [I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_sam_rh71_ek_freertos MH3-18905
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_rh71_ek_freertos/definitions.h
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_rh71_ek_freertos/definitions.h
@@ -54,10 +54,10 @@
 #include "peripheral/flexcom/twi/plib_flexcom9_twi.h"
 #include "FreeRTOS.h"
 #include "task.h"
+#include "driver/i2c/drv_i2c.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
 #include "osal/osal.h"
-#include "driver/i2c/drv_i2c.h"
 #include "bsp/bsp.h"
 #include "app_i2c_eeprom.h"
 
