8d28e4a07254df17912f95db5a88bae6026212d0
Amit Raddi <amit.raddi@microchip.com>
2019-09-03 12:56:01 +0530

    [FS] Regenerated demo for fs_sdspi_fat_sam_l21_xpro_freertos
    MH3-19602
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/fs/sdspi_fat/firmware/src/config/sam_l21_xpro_freertos/initialization.c
+++ apps/fs/sdspi_fat/firmware/src/config/sam_l21_xpro_freertos/initialization.c
@@ -220,7 +220,7 @@ const SYS_TIME_INIT sysTimeInitData =
 
 void SYS_Initialize ( void* data )
 {
-    NVMCTRL_Initialize( );
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
     PM_Initialize();
 
@@ -230,6 +230,8 @@ void SYS_Initialize ( void* data )
 
     CLOCK_Initialize();
 
+
+    NVMCTRL_Initialize( );
 
     EVSYS_Initialize();
 
