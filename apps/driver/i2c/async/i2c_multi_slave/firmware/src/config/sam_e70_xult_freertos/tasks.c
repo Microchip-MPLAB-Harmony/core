39291d1160b9a732dd648e98d1d191b4b4308251
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-09-03 15:31:32 +0530

    [I2C] Regenerated demo for driver_i2c_async_i2c_multi_slave_sam_e70_xult_freertos MH3-18873
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_e70_xult_freertos/tasks.c
+++ apps/driver/i2c/async/i2c_multi_slave/firmware/src/config/sam_e70_xult_freertos/tasks.c
@@ -53,6 +53,7 @@
 #include "configuration.h"
 #include "definitions.h"
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: RTOS "Tasks" Routine
@@ -96,7 +97,6 @@ void _APP_I2C_TEMP_SENSOR_Tasks(  void *pvParameters  )
   Remarks:
     See prototype in system/common/sys_module.h.
 */
-
 void SYS_Tasks ( void )
 {
     /* Maintain system services */
@@ -135,7 +135,6 @@ void SYS_Tasks ( void )
     vTaskStartScheduler(); /* This function never returns. */
 
 }
-
 
 /*******************************************************************************
  End of File
