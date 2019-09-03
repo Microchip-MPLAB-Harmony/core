ca58721a973b66a89fc06738f17f9ebb85ecd9d0
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2019-09-03 18:38:40 +0530

    [SDMMC] Regenerated demo for driver_sdmmc_async_sdmmc_read_write_sam_9x60_ek_freertos MH3-18652
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_9x60_ek_freertos/initialization.c
+++ apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_9x60_ek_freertos/initialization.c
@@ -174,9 +174,13 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
 	PIT_TimerInitialize();
 
-MMU_Initialize();
+    MMU_Initialize();
 
     INT_Initialize();
+    
+    /* Disable WDT   */
+    WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
+
 	SDMMC0_Initialize();
 
  
