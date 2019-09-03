3a83a90e0a08b1e0b6b48fafd107912e06a71eee
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-09-03 17:54:32 +0530

    [USART] Regenerated demo for driver_usart_async_usart_echo_sam_da1_xpro
    MH3-19321
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/async/usart_echo/firmware/src/config/sam_da1_xpro/initialization.c
+++ apps/driver/usart/async/usart_echo/firmware/src/config/sam_da1_xpro/initialization.c
@@ -183,7 +183,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
-    NVMCTRL_Initialize( );
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
   
     PORT_Initialize();
@@ -192,6 +192,8 @@ void SYS_Initialize ( void* data )
 
 
     SERCOM3_USART_Initialize();
+
+    NVMCTRL_Initialize( );
 
     EVSYS_Initialize();
 
