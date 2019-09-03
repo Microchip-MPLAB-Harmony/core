50475888964612e81cad336bd2d1d451f77f8fc3
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-09-03 17:14:57 +0530

    [USART] Regenerated demo for driver_usart_async_usart_multi_instance_sam_d21_xpro
    MH3-19353
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_d21_xpro/initialization.c
+++ apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_d21_xpro/initialization.c
@@ -249,7 +249,7 @@ SYSTEM_OBJECTS sysObj;
 
 void SYS_Initialize ( void* data )
 {
-    NVMCTRL_Initialize( );
+    NVMCTRL_REGS->NVMCTRL_CTRLB = NVMCTRL_CTRLB_RWS(3);
 
   
     PORT_Initialize();
@@ -260,6 +260,8 @@ void SYS_Initialize ( void* data )
     SERCOM3_USART_Initialize();
 
 	BSP_Initialize();
+    NVMCTRL_Initialize( );
+
     EVSYS_Initialize();
 
     SERCOM4_USART_Initialize();
