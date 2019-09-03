82fc4d1a825d262c4313cec8ae91ea6f9adf84ce
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-09-03 14:53:00 +0530

    [I2C] Regenerated demo for driver_i2c_async_i2c_eeprom_pic32mk_gp_db MH3-18831
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/pic32mk_gp_db/initialization.c
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/pic32mk_gp_db/initialization.c
@@ -141,11 +141,15 @@ const DRV_I2C_PLIB_INTERFACE drvI2C0PLibAPI = {
     /* I2C PLib Transfer Write Add function */
     .write = (DRV_I2C_PLIB_WRITE)I2C_BB_Write,
 
+
     /* I2C PLib Transfer Write Read Add function */
     .writeRead = (DRV_I2C_PLIB_WRITE_READ)I2C_BB_WriteRead,
 
     /* I2C PLib Transfer Status function */
     .errorGet = (DRV_I2C_PLIB_ERROR_GET)I2C_BB_ErrorGet,
+
+    /* I2C PLib Transfer Setup function */
+    .transferSetup = (DRV_I2C_PLIB_TRANSFER_SETUP)I2C_BB_TransferSetup,
 
     /* I2C PLib Callback Register */
     .callbackRegister = (DRV_I2C_PLIB_CALLBACK_REGISTER)I2C_BB_CallbackRegister,
