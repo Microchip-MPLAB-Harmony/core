0fbc8945cabc747cf4e8aa9a8c5084e23db1a231
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2019-09-03 18:41:35 +0530

    [SPI_EEPROM] Regenerated demo for driver_spi_eeprom_at25_at25_eeprom_read_write_sam_9x60_ek_freertos MH3-18688
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/sam_9x60_ek_freertos/initialization.c
+++ apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/sam_9x60_ek_freertos/initialization.c
@@ -153,9 +153,13 @@ void SYS_Initialize ( void* data )
 	BSP_Initialize();
 	PIT_TimerInitialize();
 
-MMU_Initialize();
+    MMU_Initialize();
 
     INT_Initialize();
+    
+    /* Disable WDT   */
+    WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
+
     FLEXCOM4_SPI_Initialize();
 
 
