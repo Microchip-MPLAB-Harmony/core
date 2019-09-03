b5a06d79094a566b5cf30066c8441dabfc09c478
Amit Raddi <amit.raddi@microchip.com>
2019-09-03 13:08:29 +0530

    [FS] Regenerated demo for fs_nvm_sdspi_fat_multi_disk_sam_e70_xult_freertos
    MH3-19550
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/fs/nvm_sdspi_fat_multi_disk/firmware/src/config/sam_e70_xult_freertos/tasks.c
+++ apps/fs/nvm_sdspi_fat_multi_disk/firmware/src/config/sam_e70_xult_freertos/tasks.c
@@ -53,6 +53,7 @@
 #include "configuration.h"
 #include "definitions.h"
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: RTOS "Tasks" Routine
@@ -105,7 +106,6 @@ void _APP_Tasks(  void *pvParameters  )
   Remarks:
     See prototype in system/common/sys_module.h.
 */
-
 void SYS_Tasks ( void )
 {
     /* Maintain system services */
@@ -154,7 +154,6 @@ void SYS_Tasks ( void )
     vTaskStartScheduler(); /* This function never returns. */
 
 }
-
 
 /*******************************************************************************
  End of File
