dd83da6f4170136e6539b8cb82085ad18ac61ba8
Thimmarayaswamy Krishnappa <thimmarayaswamy.krishnappa@microchip.com>
2019-09-03 14:56:53 +0530

    [I2C] Regenerated demo for driver_i2c_async_i2c_eeprom_sam_e54_xpro MH3-18845
    Signed-off-by: Thimmarayaswamy Krishnappa <Thimmarayaswamy.Krishnappa@microchip.com>

--- apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_e54_xpro/initialization.c
+++ apps/driver/i2c/async/i2c_eeprom/firmware/src/config/sam_e54_xpro/initialization.c
@@ -95,11 +95,15 @@ const DRV_I2C_PLIB_INTERFACE drvI2C0PLibAPI = {
     /* I2C PLib Transfer Write Add function */
     .write = (DRV_I2C_PLIB_WRITE)SERCOM7_I2C_Write,
 
+
     /* I2C PLib Transfer Write Read Add function */
     .writeRead = (DRV_I2C_PLIB_WRITE_READ)SERCOM7_I2C_WriteRead,
 
     /* I2C PLib Transfer Status function */
     .errorGet = (DRV_I2C_PLIB_ERROR_GET)SERCOM7_I2C_ErrorGet,
+
+    /* I2C PLib Transfer Setup function */
+    .transferSetup = (DRV_I2C_PLIB_TRANSFER_SETUP)SERCOM7_I2C_TransferSetup,
 
     /* I2C PLib Callback Register */
     .callbackRegister = (DRV_I2C_PLIB_CALLBACK_REGISTER)SERCOM7_I2C_CallbackRegister,
