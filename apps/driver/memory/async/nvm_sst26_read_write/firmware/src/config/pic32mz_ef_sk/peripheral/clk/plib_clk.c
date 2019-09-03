259a39cbd3a4ce4a06bf0d54306872c15c1c4f93
Amit Raddi <amit.raddi@microchip.com>
2019-09-03 12:05:33 +0530

    [MEMORY] Regenerated demo for driver_memory_async_nvm_sst26_read_write_pic32mz_ef_sk
    MH3-18951
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/memory/async/nvm_sst26_read_write/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
+++ apps/driver/memory/async/nvm_sst26_read_write/firmware/src/config/pic32mz_ef_sk/peripheral/clk/plib_clk.c
@@ -112,11 +112,11 @@ void CLK_Initialize( void )
   
 
     /* Peripheral Module Disable Configuration */
-    PMD1 = 0xffffffff;
-    PMD2 = 0xffffffff;
-    PMD3 = 0xffffffff;
-    PMD4 = 0xffffffff;
-    PMD5 = 0xfeffffff;
+    PMD1 = 0x1001;
+    PMD2 = 0x3;
+    PMD3 = 0x1ff01ff;
+    PMD4 = 0x1ff;
+    PMD5 = 0x301f3f3f;
     PMD6 = 0xff7ff0ff;
     PMD7 = 0xffffffef;
 
