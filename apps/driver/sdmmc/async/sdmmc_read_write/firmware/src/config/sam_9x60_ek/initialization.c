81d423b502787b116b3e827d99f853abb5c38297
Jignesh Moradiya <jignesh.moradiya@microchip.com>
2019-09-03 18:38:15 +0530

    [SDMMC] Regenerated demo for driver_sdmmc_async_sdmmc_read_write_sam_9x60_ek MH3-18650
    Signed-off-by: Jignesh Moradiya <jignesh.moradiya@microchip.com>

--- apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_9x60_ek/initialization.c
+++ apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_9x60_ek/initialization.c
@@ -172,9 +172,13 @@ void SYS_Initialize ( void* data )
 	PIO_Initialize();
 
 	BSP_Initialize();
-MMU_Initialize();
+    MMU_Initialize();
 
     INT_Initialize();
+    
+    /* Disable WDT   */
+    WDT_REGS->WDT_MR = WDT_MR_WDDIS_Msk;
+
 	SDMMC0_Initialize();
 
  
