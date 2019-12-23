4fad05f508f5524b3721f1bd100ff967307ed630
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-12-23 16:51:40 +0530

    [I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_pic32mx470_curiosity_freertos
    MH3-25107
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/pic32mx470_curiosity_freertos/initialization.c
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/pic32mx470_curiosity_freertos/initialization.c
@@ -190,7 +190,6 @@ void SYS_Initialize ( void* data )
 
   
     CLK_Initialize();
-	GPIO_Initialize();
 
     /* Configure KSEG0 as cacheable memory. This is needed for Prefetch Buffer */
     __builtin_mtc0(16, 0,(__builtin_mfc0(16, 0) | 0x3));
@@ -204,6 +203,9 @@ void SYS_Initialize ( void* data )
 
 
 
+
+
+	GPIO_Initialize();
 
     I2C1_Initialize();
 	BSP_Initialize();
