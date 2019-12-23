f9eac751c174195e354b3a2f67c09f33835acc45
Amit Raddi <amit.raddi@microchip.com>
2019-12-23 16:15:50 +0530

    [TIME] Regenerated demo for system_time_sys_time_multiclient_sam_e54_xpro
    MH3-26812
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_e54_xpro/definitions.h
@@ -56,6 +56,7 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "system/int/sys_int.h"
 #include "system/cache/sys_cache.h"
