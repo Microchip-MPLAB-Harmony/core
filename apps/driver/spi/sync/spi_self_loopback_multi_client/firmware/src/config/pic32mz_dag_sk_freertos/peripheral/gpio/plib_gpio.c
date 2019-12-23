aae9b45f1b4eed6b37e6581fc1e56bcdf91cab66
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-12-23 16:39:01 +0530

    [SPI] Regenerated demo for driver_spi_sync_spi_self_loopback_multi_client_pic32mz_dag_sk_freertos
    MH3-25622
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/sync/spi_self_loopback_multi_client/firmware/src/config/pic32mz_dag_sk_freertos/peripheral/gpio/plib_gpio.c
+++ apps/driver/spi/sync/spi_self_loopback_multi_client/firmware/src/config/pic32mz_dag_sk_freertos/peripheral/gpio/plib_gpio.c
@@ -86,7 +86,7 @@ void GPIO_Initialize ( void )
     ANSELKCLR = 0x6; /* Digital Mode Enable */
 
 
-    /* unlock system for PPS configuration */
+    /* Unlock system for PPS configuration */
     SYSKEY = 0x00000000;
     SYSKEY = 0xAA996655;
     SYSKEY = 0x556699AA;
