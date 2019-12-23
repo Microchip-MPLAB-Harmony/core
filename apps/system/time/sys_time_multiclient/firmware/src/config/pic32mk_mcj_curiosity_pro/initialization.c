d9d0e891c221af84fdc2b7764453b034f9999989
Amit Raddi <amit.raddi@microchip.com>
2019-12-23 16:09:53 +0530

    [TIME] Regenerated demo for system_time_sys_time_multiclient_pic32mk_mcj_curiosity_pro
    MH3-26764
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/pic32mk_mcj_curiosity_pro/initialization.c
+++ apps/system/time/sys_time_multiclient/firmware/src/config/pic32mk_mcj_curiosity_pro/initialization.c
@@ -140,11 +140,12 @@ SYSTEM_OBJECTS sysObj;
 
 const SYS_TIME_PLIB_INTERFACE sysTimePlibAPI = {
     .timerCallbackSet = (SYS_TIME_PLIB_CALLBACK_REGISTER)CORETIMER_CallbackSet,
-    .timerCounterGet = (SYS_TIME_PLIB_COUNTER_GET)CORETIMER_CounterGet,
-    .timerFrequencyGet = (SYS_TIME_PLIB_FREQUENCY_GET)CORETIMER_FrequencyGet,
-    .timerCompareSet = (SYS_TIME_PLIB_COMPARE_SET)CORETIMER_CompareSet,
     .timerStart = (SYS_TIME_PLIB_START)CORETIMER_Start,
-    .timerStop = (SYS_TIME_PLIB_STOP)CORETIMER_Stop 
+    .timerStop = (SYS_TIME_PLIB_STOP)CORETIMER_Stop ,
+    .timerFrequencyGet = (SYS_TIME_PLIB_FREQUENCY_GET)CORETIMER_FrequencyGet,
+    .timerPeriodSet = (SYS_TIME_PLIB_PERIOD_SET)NULL,
+    .timerCompareSet = (SYS_TIME_PLIB_COMPARE_SET)CORETIMER_CompareSet,
+    .timerCounterGet = (SYS_TIME_PLIB_COUNTER_GET)CORETIMER_CounterGet,
 };
 
 const SYS_TIME_INIT sysTimeInitData =
@@ -174,7 +175,6 @@ void SYS_Initialize ( void* data )
 
   
     CLK_Initialize();
-	GPIO_Initialize();
 
     /* Configure CP0.K0 for optimal performance (cached instruction pre-fetch) */
     __builtin_mtc0(16, 0,(__builtin_mfc0(16, 0) | 0x3));
@@ -183,6 +183,9 @@ void SYS_Initialize ( void* data )
     CHECONbits.PFMWS = 3;
     CHECONbits.PREFEN = 1;
 
+
+
+	GPIO_Initialize();
 
     CORETIMER_Initialize();
 	UART2_Initialize();
