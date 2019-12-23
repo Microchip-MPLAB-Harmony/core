486997ce3573df370287b1c9f19ca540696b2ddf
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-12-23 16:37:14 +0530

    [SPI] Regenerated demo for driver_spi_sync_spi_self_loopback_multi_client_pic32mk_mcj_curiosity_pro_freertos
    MH3-25614
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/sync/spi_self_loopback_multi_client/firmware/src/config/pic32mk_mcj_curiosity_pro_freertos/initialization.c
+++ apps/driver/spi/sync/spi_self_loopback_multi_client/firmware/src/config/pic32mk_mcj_curiosity_pro_freertos/initialization.c
@@ -208,7 +208,6 @@ void SYS_Initialize ( void* data )
 
   
     CLK_Initialize();
-	GPIO_Initialize();
 
     /* Configure CP0.K0 for optimal performance (cached instruction pre-fetch) */
     __builtin_mtc0(16, 0,(__builtin_mfc0(16, 0) | 0x3));
@@ -217,6 +216,9 @@ void SYS_Initialize ( void* data )
     CHECONbits.PFMWS = 3;
     CHECONbits.PREFEN = 1;
 
+
+
+	GPIO_Initialize();
 
 	SPI2_Initialize();
 
