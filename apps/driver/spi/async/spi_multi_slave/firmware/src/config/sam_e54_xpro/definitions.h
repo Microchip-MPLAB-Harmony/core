c2014390ec7756f76ac62f29f0afef74e9d73b24
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-12-23 15:57:23 +0530

    [SPI] Regenerated demo for driver_spi_async_spi_multi_slave_sam_e54_xpro
    MH3-25458
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_e54_xpro/definitions.h
@@ -56,6 +56,7 @@
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
 #include "peripheral/dmac/plib_dmac.h"
+#include "peripheral/cmcc/plib_cmcc.h"
 #include "driver/spi/drv_spi.h"
 #include "system/int/sys_int.h"
 #include "system/ports/sys_ports.h"
--- apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_e54_xpro/initialization.c
+++ apps/driver/spi/async/spi_multi_slave/firmware/src/config/sam_e54_xpro/initialization.c
@@ -207,6 +207,8 @@ void SYS_Initialize ( void* data )
     CLOCK_Initialize();
 
 
+
+
 	BSP_Initialize();
     EVSYS_Initialize();
 
