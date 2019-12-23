eca05c06031e156745e1d4c5835e09fe07d85def
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-12-23 17:01:10 +0530

    [I2C] Regenerated demo for driver_i2c_sync_i2c_multi_slave_sam_g55_xpro_freertos
    MH3-25131
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/sync/i2c_multi_slave/firmware/src/config/sam_g55_xpro_freertos/definitions.h
+++ apps/driver/i2c/sync/i2c_multi_slave/firmware/src/config/sam_g55_xpro_freertos/definitions.h
@@ -52,6 +52,7 @@
 #include "peripheral/pio/plib_pio.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/efc/plib_efc.h"
 #include "peripheral/flexcom/twi/plib_flexcom6_twi.h"
 #include "driver/i2c/drv_i2c.h"
