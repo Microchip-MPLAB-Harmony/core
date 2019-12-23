5f5f1647ad38f14846299ac5acd4bc3c6a7c1b98
Vishal Gandhi <vishal.gandhi@microchip.com>
2019-12-23 16:50:56 +0530

    [I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_pic32mk_mcm_curiosity_pro_freertos
    MH3-25105
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/pic32mk_mcm_curiosity_pro_freertos/initialization.c
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/pic32mk_mcm_curiosity_pro_freertos/initialization.c
@@ -213,7 +213,6 @@ void SYS_Initialize ( void* data )
 
   
     CLK_Initialize();
-	GPIO_Initialize();
 
     /* Configure CP0.K0 for optimal performance (cached instruction pre-fetch) */
     __builtin_mtc0(16, 0,(__builtin_mfc0(16, 0) | 0x3));
@@ -222,6 +221,9 @@ void SYS_Initialize ( void* data )
     CHECONbits.PFMWS = 3;
     CHECONbits.PREFEN = 1;
 
+
+
+	GPIO_Initialize();
 
 	BSP_Initialize();
     I2C4_Initialize();
