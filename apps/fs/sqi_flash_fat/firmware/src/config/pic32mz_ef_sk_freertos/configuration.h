cefc014dc060cbf5c87b27d2986252e0da51121e
Amit Raddi <amit.raddi@microchip.com>
2019-12-24 16:06:49 +0530

    [FS] Regenerated demo for fs_sqi_flash_fat_pic32mz_ef_sk_freertos
    MH3-26505
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/fs/sqi_flash_fat/firmware/src/config/pic32mz_ef_sk_freertos/configuration.h
+++ apps/fs/sqi_flash_fat/firmware/src/config/pic32mz_ef_sk_freertos/configuration.h
@@ -106,7 +106,7 @@ extern "C" {
 #define SYS_TIME_MAX_TIMERS                  5
 #define SYS_TIME_HW_COUNTER_WIDTH            32
 #define SYS_TIME_HW_COUNTER_PERIOD           4294967295U
-#define SYS_TIME_HW_COUNTER_HALF_PERIOD		 (SYS_TIME_HW_COUNTER_PERIOD>>1)
+#define SYS_TIME_HW_COUNTER_HALF_PERIOD	     (SYS_TIME_HW_COUNTER_PERIOD>>1)
 #define SYS_TIME_CPU_CLOCK_FREQUENCY         200000000
 #define SYS_TIME_COMPARE_UPDATE_EXECUTION_CYCLES      (620)
 
@@ -125,6 +125,7 @@ extern "C" {
 /* Memory Driver Instance 0 RTOS Configurations*/
 #define DRV_MEMORY_STACK_SIZE_IDX0           1024
 #define DRV_MEMORY_PRIORITY_IDX0             1
+#define DRV_MEMORY_RTOS_DELAY_IDX0                         10
 
 /* Memory Driver Global Configuration Options */
 #define DRV_MEMORY_INSTANCES_NUMBER          1
