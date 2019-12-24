9ad8b7bafc88747d1b96f2da52cc71df6b04a72a
Amit Raddi <amit.raddi@microchip.com>
2019-12-24 15:56:50 +0530

    [FS] Regenerated demo for fs_sdspi_fat_pic32mx470_curiosity
    MH3-26429
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/fs/sdspi_fat/firmware/src/config/pic32mx470_curiosity/initialization.c
+++ apps/fs/sdspi_fat/firmware/src/config/pic32mx470_curiosity/initialization.c
@@ -228,11 +228,12 @@ const SYS_FS_REGISTRATION_TABLE sysFSInit [ SYS_FS_MAX_FILE_SYSTEM_TYPE ] =
 
 const SYS_TIME_PLIB_INTERFACE sysTimePlibAPI = {
     .timerCallbackSet = (SYS_TIME_PLIB_CALLBACK_REGISTER)CORETIMER_CallbackSet,
-    .timerCounterGet = (SYS_TIME_PLIB_COUNTER_GET)CORETIMER_CounterGet,
-    .timerFrequencyGet = (SYS_TIME_PLIB_FREQUENCY_GET)CORETIMER_FrequencyGet,
-    .timerCompareSet = (SYS_TIME_PLIB_COMPARE_SET)CORETIMER_CompareSet,
     .timerStart = (SYS_TIME_PLIB_START)CORETIMER_Start,
-    .timerStop = (SYS_TIME_PLIB_STOP)CORETIMER_Stop 
+    .timerStop = (SYS_TIME_PLIB_STOP)CORETIMER_Stop ,
+    .timerFrequencyGet = (SYS_TIME_PLIB_FREQUENCY_GET)CORETIMER_FrequencyGet,
+    .timerPeriodSet = (SYS_TIME_PLIB_PERIOD_SET)NULL,
+    .timerCompareSet = (SYS_TIME_PLIB_COMPARE_SET)CORETIMER_CompareSet,
+    .timerCounterGet = (SYS_TIME_PLIB_COUNTER_GET)CORETIMER_CounterGet,
 };
 
 const SYS_TIME_INIT sysTimeInitData =
@@ -262,7 +263,6 @@ void SYS_Initialize ( void* data )
 
   
     CLK_Initialize();
-	GPIO_Initialize();
 
     /* Configure KSEG0 as cacheable memory. This is needed for Prefetch Buffer */
     __builtin_mtc0(16, 0,(__builtin_mfc0(16, 0) | 0x3));
@@ -276,6 +276,9 @@ void SYS_Initialize ( void* data )
 
 
 
+
+
+	GPIO_Initialize();
 
     CORETIMER_Initialize();
 	BSP_Initialize();
