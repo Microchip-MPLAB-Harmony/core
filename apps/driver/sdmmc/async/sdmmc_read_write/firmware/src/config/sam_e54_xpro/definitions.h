3ef57cf77fa9406bfb7c92e8dd185e7e54d64a7b
Amit Raddi <amit.raddi@microchip.com>
2019-12-24 15:25:22 +0530

    [SDMMC] Regenerated demo for driver_sdmmc_async_sdmmc_read_write_sam_e54_xpro
    MH3-25358
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_e54_xpro/definitions.h
+++ apps/driver/sdmmc/async/sdmmc_read_write/firmware/src/config/sam_e54_xpro/definitions.h
@@ -55,6 +55,7 @@
 #include "peripheral/port/plib_port.h"
 #include "peripheral/clock/plib_clock.h"
 #include "peripheral/nvic/plib_nvic.h"
+#include "peripheral/cmcc/plib_cmcc.h"
 #include "peripheral/tc/plib_tc0.h"
 #include "peripheral/sdhc/plib_sdhc1.h"
 #include "system/int/sys_int.h"
