660f5528715faff3c3d330c41cbaf1c5a57adec5
Syam Nair <syam.nair@microchip.com>
2020-01-09 15:41:19 +0530

    [FreeRTOS] Regenerated all Sam9x6 core demos
    MH3-28618

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_9x60_ek_freertos_xc32/system/int/src/sys_int.c
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_9x60_ek_freertos_xc32/system/int/src/sys_int.c
@@ -137,10 +137,12 @@ INT_IsInterruptEnabled( INT_SOURCE aSrcSelection )
 {
     bool retval = false;
     aic_registers_t * aicPtr = _aicInstanceGet( aSrcSelection );
+    bool processorStatus = INT_IrqDisable();
     aicPtr->AIC_SSR = AIC_SSR_INTSEL( (uint32_t) aSrcSelection );
     if( aicPtr->AIC_IMR & AIC_IMR_Msk ){
         retval = true;
     }
+    INT_IrqRestore(processorStatus);
     return( retval );
 }
 
@@ -148,17 +150,19 @@ void
 INT_InterruptEnable( INT_SOURCE aSrcSelection )
 {
     aic_registers_t * aicPtr = _aicInstanceGet( aSrcSelection );
+    bool processorStatus = INT_IrqDisable();
     aicPtr->AIC_SSR = AIC_SSR_INTSEL( (uint32_t) aSrcSelection );
     aicPtr->AIC_IECR = AIC_IECR_Msk;
+    INT_IrqRestore(processorStatus);
     return;
 }
 
 bool
 INT_InterruptDisable( INT_SOURCE aSrcSelection )
 {
-    bool processorStatus = INT_IrqDisable();
     bool previousValue = INT_IsInterruptEnabled( aSrcSelection );
     aic_registers_t * aicPtr = _aicInstanceGet( aSrcSelection );
+    bool processorStatus = INT_IrqDisable();
     aicPtr->AIC_SSR = AIC_SSR_INTSEL( (uint32_t) aSrcSelection );
     aicPtr->AIC_IDCR = AIC_IDCR_Msk;
     __DSB();
@@ -170,10 +174,13 @@ INT_InterruptDisable( INT_SOURCE aSrcSelection )
 void
 INT_InterruptRestore( INT_SOURCE aSrcSelection, bool state )
 {
-    if( state ) {
+    if( state ) 
+    {
         aic_registers_t * aicPtr = _aicInstanceGet( aSrcSelection );
+        bool processorStatus = INT_IrqDisable();
         aicPtr->AIC_SSR = AIC_SSR_INTSEL( (uint32_t) aSrcSelection );
         aicPtr->AIC_IECR = AIC_IECR_Msk;
+        INT_IrqRestore(processorStatus);
     }
     return;
 }
@@ -188,6 +195,7 @@ INT_IsInterruptPendingFor( INT_SOURCE aSrcSelection )
         uint32_t regSelection = aSrcSelection >> 5; // 32 status bits per register 
         uint32_t bitSelection = aSrcSelection - (regSelection << 5);
         aic_registers_t * aicPtr = _aicInstanceGet( aSrcSelection );
+        bool processorStatus = INT_IrqDisable();
         aicPtr->AIC_SSR = AIC_SSR_INTSEL( (uint32_t) aSrcSelection );
         switch( regSelection )    
         {
@@ -205,6 +213,7 @@ INT_IsInterruptPendingFor( INT_SOURCE aSrcSelection )
             // regValue = aicPtr->AIC_IPR3;
             break;
         }
+        INT_IrqRestore(processorStatus);
         if( regValue & (0x00000001Ul << bitSelection) ) {
             retval = true;
         }
@@ -216,8 +225,10 @@ void
 INT_InterruptPendingSet( INT_SOURCE aSrcSelection )
 {
     aic_registers_t * aicPtr = _aicInstanceGet( aSrcSelection );
+    bool processorStatus = INT_IrqDisable();
     aicPtr->AIC_SSR = AIC_SSR_INTSEL( (uint32_t) aSrcSelection );
     aicPtr->AIC_ISCR = AIC_ISCR_Msk;
+    INT_IrqRestore(processorStatus);
     return;
 }
 
@@ -225,7 +236,9 @@ void
 INT_InterruptPendingClear( INT_SOURCE aSrcSelection )
 {
     aic_registers_t * aicPtr = _aicInstanceGet( aSrcSelection );
+    bool processorStatus = INT_IrqDisable();
     aicPtr->AIC_SSR = AIC_SSR_INTSEL( (uint32_t) aSrcSelection );
     aicPtr->AIC_ICCR = AIC_ICCR_Msk;
+    INT_IrqRestore(processorStatus);
     return;
 }
