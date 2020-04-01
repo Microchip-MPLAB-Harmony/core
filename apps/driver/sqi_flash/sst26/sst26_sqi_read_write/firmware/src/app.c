5e8ec2ecba4f43c522b40e3964752bcb4da62c9f
Amit Raddi <amit.raddi@microchip.com>
2020-04-01 17:57:59 +0530

    [DRV_SST26] Renamed sst26_flash_read_write application to sst26_sqi_read_write
    - Modified application file for sector erase check
    
    MH3-31394
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

---
+++ apps/driver/sqi_flash/sst26/sst26_sqi_read_write/firmware/src/app.c
@@ -0,0 +1,313 @@
+/*******************************************************************************
+  MPLAB Application Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    app.c
+
+  Summary:
+    This file contains the source code for the MPLAB application.
+
+  Description:
+    This file contains the source code for the MPLAB application.  It
+    implements the logic of the application's state machine and it may call
+    API routines of other MPLAB modules in the system, such as drivers,
+    system services, and middleware.  However, it does not call any of the
+    system interfaces (such as the "Initialize" and "Tasks" functions) of any of
+    the modules in the system or make any assumptions about when those functions
+    are called.  That is the responsibility of the configuration-specific system
+    files.
+ *******************************************************************************/
+
+// DOM-IGNORE-BEGIN
+/*******************************************************************************
+* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+*
+* Subject to your compliance with these terms, you may use Microchip software
+* and any derivatives exclusively with Microchip products. It is your
+* responsibility to comply with third party license terms applicable to your
+* use of third party software (including open source software) that may
+* accompany Microchip software.
+*
+* THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER
+* EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY IMPLIED
+* WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS FOR A
+* PARTICULAR PURPOSE.
+*
+* IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE,
+* INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND
+* WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP HAS
+* BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO THE
+* FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
+* ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
+* THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
+*******************************************************************************/
+// DOM-IGNORE-END
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Included Files
+// *****************************************************************************
+// *****************************************************************************
+
+#include <string.h>
+#include "app.h"
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Global Data Definitions
+// *****************************************************************************
+// *****************************************************************************
+
+// *****************************************************************************
+/* Application Data
+
+  Summary:
+    Holds application data
+
+  Description:
+    This structure holds the application's data.
+
+  Remarks:
+    This structure should be initialized by the APP_Initialize function.
+
+    Application strings and buffers are be defined outside this structure.
+*/
+
+APP_DATA CACHE_ALIGN appData;
+
+static uint32_t erase_index = 0;
+static uint32_t write_index = 0;
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Callback Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/* TODO:  Add any necessary callback functions.
+*/
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Local Functions
+// *****************************************************************************
+// *****************************************************************************
+
+
+/* TODO:  Add any necessary local functions.
+*/
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Application Initialization and State Machine Functions
+// *****************************************************************************
+// *****************************************************************************
+
+/*******************************************************************************
+  Function:
+    void APP_Initialize ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+
+void APP_Initialize ( void )
+{
+    uint32_t i = 0;
+
+    /* Place the App state machine in its initial state. */
+    appData.state = APP_STATE_INIT;
+
+    for (i = 0; i < BUFFER_SIZE; i++)
+        appData.writeBuffer[i] = i;
+}
+
+
+/******************************************************************************
+  Function:
+    void APP_Tasks ( void )
+
+  Remarks:
+    See prototype in app.h.
+ */
+void APP_Tasks ( void )
+{
+    DRV_SST26_TRANSFER_STATUS transferStatus = DRV_SST26_TRANSFER_ERROR_UNKNOWN;
+
+    /* Check the application's current state. */
+    switch ( appData.state )
+    {
+        /* Application's initial state. */
+        case APP_STATE_INIT:
+        {
+            if (DRV_SST26_Status(DRV_SST26_INDEX) == SYS_STATUS_READY)
+            {
+                appData.state = APP_STATE_OPEN_DRIVER;
+            }
+            break;
+        }
+
+        case APP_STATE_OPEN_DRIVER:
+        {
+            appData.handle = DRV_SST26_Open(DRV_SST26_INDEX, DRV_IO_INTENT_READWRITE);
+
+            if (appData.handle != DRV_HANDLE_INVALID)
+            {
+                appData.state = APP_STATE_GEOMETRY_GET;
+            }
+
+            break;
+        }
+        case APP_STATE_GEOMETRY_GET:
+        {
+            if (DRV_SST26_GeometryGet(appData.handle, &appData.geometry) != true)
+            {
+                appData.state = APP_STATE_ERROR;
+                break;
+            }
+
+            erase_index = 0;
+            write_index = 0;
+            appData.state = APP_STATE_ERASE_FLASH;
+
+            break;
+        }
+
+        case APP_STATE_ERASE_FLASH:
+        {
+            if (DRV_SST26_SectorErase(appData.handle, (MEM_ADDRESS + erase_index)) != true)
+            {
+                appData.state = APP_STATE_ERROR;
+            }
+
+            appData.state = APP_STATE_ERASE_WAIT;
+
+            break;
+        }
+
+        case APP_STATE_ERASE_WAIT:
+        {
+            transferStatus = DRV_SST26_TransferStatusGet(appData.handle);
+
+            if(transferStatus == DRV_SST26_TRANSFER_COMPLETED)
+            {
+                erase_index += appData.geometry.erase_blockSize;
+
+                if (erase_index < BUFFER_SIZE)
+                {
+                    appData.state = APP_STATE_ERASE_FLASH;
+                }
+                else
+                {
+                    appData.state = APP_STATE_WRITE_MEMORY;
+                }
+            }
+            else if (transferStatus == DRV_SST26_TRANSFER_ERROR_UNKNOWN)
+            {
+                appData.state = APP_STATE_ERROR;
+            }
+            break;
+        }
+
+        case APP_STATE_WRITE_MEMORY:
+        {
+            if (DRV_SST26_PageWrite(appData.handle, (uint32_t *)&appData.writeBuffer[write_index], (MEM_ADDRESS + write_index)) != true)
+            {
+                appData.state = APP_STATE_ERROR;
+                break;
+            }
+
+            appData.state = APP_STATE_WRITE_WAIT;
+
+            break;
+        }
+
+        case APP_STATE_WRITE_WAIT:
+        {
+            transferStatus = DRV_SST26_TransferStatusGet(appData.handle);
+
+            if(transferStatus == DRV_SST26_TRANSFER_COMPLETED)
+            {
+                write_index += appData.geometry.write_blockSize;
+
+                if (write_index < BUFFER_SIZE)
+                {
+                    appData.state = APP_STATE_WRITE_MEMORY;
+                }
+                else
+                {
+                    appData.state = APP_STATE_READ_MEMORY;
+                }
+            }
+            else if (transferStatus == DRV_SST26_TRANSFER_ERROR_UNKNOWN)
+            {
+                appData.state = APP_STATE_ERROR;
+            }
+
+            break;
+        }
+
+        case APP_STATE_READ_MEMORY:
+        {
+            if (DRV_SST26_Read(appData.handle, (uint32_t *)&appData.readBuffer, BUFFER_SIZE, MEM_ADDRESS) != true)
+            {
+                appData.state = APP_STATE_ERROR;
+            }
+            else
+            {
+                appData.state = APP_STATE_READ_WAIT;
+            }
+
+            break;
+        }
+
+        case APP_STATE_READ_WAIT:
+        {
+            transferStatus = DRV_SST26_TransferStatusGet(appData.handle);
+
+            if(transferStatus == DRV_SST26_TRANSFER_COMPLETED)
+            {
+                appData.state = APP_STATE_VERIFY_DATA;
+            }
+            else if (transferStatus == DRV_SST26_TRANSFER_ERROR_UNKNOWN)
+            {
+                appData.state = APP_STATE_ERROR;
+            }
+
+            break;
+        }
+
+        case APP_STATE_VERIFY_DATA:
+        {
+            if (!memcmp(appData.writeBuffer, appData.readBuffer, BUFFER_SIZE))
+            {
+                appData.state = APP_STATE_SUCCESS;
+            }
+            else
+            {
+                appData.state = APP_STATE_ERROR;
+            }
+
+            break;
+        }
+
+        case APP_STATE_SUCCESS:
+        {
+            DRV_SST26_Close(appData.handle);
+
+            LED_ON();
+            break;
+        }
+
+        case APP_STATE_ERROR:
+        default:
+        {
+            DRV_SST26_Close(appData.handle);
+            break;
+        }
+    }
+}
