9d346dc806dfd64812c3c9289e2bde637eb5cb91
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-04-20 18:08:12 +0530

    [SYS_TIME] Regenerated demo for system_time_sys_time_multiclient_sam_da1_xpro
    MH3-35464
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/system/time/sys_time_multiclient/firmware/src/config/sam_da1_xpro/bsp/bsp.h
+++ apps/system/time/sys_time_multiclient/firmware/src/config/sam_da1_xpro/bsp/bsp.h
@@ -61,14 +61,17 @@
 // *****************************************************************************
 // *****************************************************************************
 
+
 /*** LED Macros for LED ***/
-#define LED_Toggle() (PORT_REGS->GROUP[1].PORT_OUTTGL = 1 << 30)
-#define LED_On() (PORT_REGS->GROUP[1].PORT_OUTCLR = 1 << 30)
-#define LED_Off() (PORT_REGS->GROUP[1].PORT_OUTSET = 1 << 30)
+#define LED_Toggle()     (PORT_REGS->GROUP[1].PORT_OUTTGL = 1 << 30)
+#define LED_On()         (PORT_REGS->GROUP[1].PORT_OUTCLR = 1 << 30)
+#define LED_Off()        (PORT_REGS->GROUP[1].PORT_OUTSET = 1 << 30)
+
 /*** SWITCH Macros for SWITCH ***/
-#define SWITCH_Get() ((PORT_REGS->GROUP[0].PORT_IN >> 15) & 0x01)
-#define SWITCH_STATE_PRESSED 0
-#define SWITCH_STATE_RELEASED 1
+#define SWITCH_Get()     ((PORT_REGS->GROUP[0].PORT_IN >> 15) & 0x01)
+#define SWITCH_STATE_PRESSED   0
+#define SWITCH_STATE_RELEASED  1
+
 
 
 
