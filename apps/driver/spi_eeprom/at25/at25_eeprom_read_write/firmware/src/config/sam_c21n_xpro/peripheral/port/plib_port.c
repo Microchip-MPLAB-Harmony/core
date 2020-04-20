19073f4270ab3d2acfee8c6328bea57b774d7443
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-04-20 16:59:07 +0530

    [DRV_SPI_EEPROM] Regenerated demo for driver_spi_eeprom_at25_at25_eeprom_read_write_sam_c21n_xpro
    MH3-34908
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/driver/spi_eeprom/at25/at25_eeprom_read_write/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -74,6 +74,7 @@ void PORT_Initialize(void)
    /************************** GROUP 0 Initialization *************************/
    PORT_REGS->GROUP[0].PORT_DIR = 0x100000;
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x40000008;
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000;
@@ -84,10 +85,12 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[1].PORT_PMUX[0] = 0x33;
    PORT_REGS->GROUP[1].PORT_PMUX[1] = 0x3;
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x20;
    PORT_REGS->GROUP[2].PORT_OUT = 0x20;
 
+                
 }
 
 // *****************************************************************************
