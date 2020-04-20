0da43d6fcdff891f0c2d9636455c3a476fc8016a
Amit Raddi <amit.raddi@microchip.com>
2020-04-20 17:02:42 +0530

    [DRV_SPI] Regenerated demo for driver_spi_sync_spi_multi_slave_sam_e70_xult_freertos
    MH3-34781
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/spi/sync/spi_multi_slave/firmware/src/config/sam_e70_xult_freertos/tasks.c
+++ apps/driver/spi/sync/spi_multi_slave/firmware/src/config/sam_e70_xult_freertos/tasks.c
@@ -93,6 +93,7 @@ void _APP_MONITOR_Tasks(  void *pvParameters  )
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System "Tasks" Routine
@@ -141,6 +142,7 @@ void SYS_Tasks ( void )
                 NULL,
                 1,
                 &xAPP_MONITOR_Tasks);
+
 
 
 
