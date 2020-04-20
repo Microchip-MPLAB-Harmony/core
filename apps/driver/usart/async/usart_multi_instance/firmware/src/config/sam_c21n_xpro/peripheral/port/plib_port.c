d9ecbe48686e7cf5c1fe40461a345e9ed88cf7a7
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-04-20 17:01:10 +0530

    [DRV_USART] Regenerated demo for driver_usart_async_usart_multi_instance_sam_c21n_xpro
    MH3-34996
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
+++ apps/driver/usart/async/usart_multi_instance/firmware/src/config/sam_c21n_xpro/peripheral/port/plib_port.c
@@ -76,6 +76,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[13] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[6] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000;
    PORT_REGS->GROUP[1].PORT_PINCFG[10] = 0x1;
@@ -83,10 +84,12 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[1].PORT_PINCFG[19] = 0x4;
 
    PORT_REGS->GROUP[1].PORT_PMUX[5] = 0x33;
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x20;
 
    PORT_REGS->GROUP[2].PORT_PMUX[2] = 0x2;
+                
 }
 
 // *****************************************************************************
