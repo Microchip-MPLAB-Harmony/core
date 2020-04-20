abf574a2299770cd105f513fb557b9489e78f0bd
Amit Raddi <amit.raddi@microchip.com>
2020-04-20 16:59:57 +0530

    [DRV_SDSPI] Regenerated demo for driver_sdspi_sync_sdspi_read_write_sam_e70_xult_freertos
    MH3-34687
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/driver/sdspi/sync/sdspi_read_write/firmware/src/config/sam_e70_xult_freertos/initialization.c
+++ apps/driver/sdspi/sync/sdspi_read_write/firmware/src/config/sam_e70_xult_freertos/initialization.c
@@ -96,6 +96,7 @@ const DRV_SDSPI_PLIB_INTERFACE drvSDSPI0PlibAPI = {
 const uint32_t drvSDSPI0remapDataBits[]= { 0x0, 0x10, 0x20, 0x30, 0x40, 0x50, 0x60, 0x70, 0x80 };
 const uint32_t drvSDSPI0remapClockPolarity[] = { 0x0, 0x1 };
 const uint32_t drvSDSPI0remapClockPhase[] = { 0x0, 0x2 };
+
 /* SDSPI Driver Initialization Data */
 const DRV_SDSPI_INIT drvSDSPI0InitData =
 {
@@ -108,6 +109,7 @@ const DRV_SDSPI_INIT drvSDSPI0InitData =
 
     .remapClockPhase        = drvSDSPI0remapClockPhase,
 
+
     /* SDSPI Number of clients */
     .numClients             = DRV_SDSPI_CLIENTS_NUMBER_IDX0,
 
@@ -118,7 +120,7 @@ const DRV_SDSPI_INIT drvSDSPI0InitData =
     .chipSelectPin          = DRV_SDSPI_CHIP_SELECT_PIN_IDX0,
 
     .sdcardSpeedHz          = DRV_SDSPI_SPEED_HZ_IDX0,
-    
+
     .pollingIntervalMs      = DRV_SDSPI_POLLING_INTERVAL_MS_IDX0,
 
     .writeProtectPin        = SYS_PORT_PIN_NONE,
@@ -181,6 +183,14 @@ const SYS_TIME_INIT sysTimeInitData =
 };
 
 // </editor-fold>
+
+
+
+// *****************************************************************************
+// *****************************************************************************
+// Section: Local initialization functions
+// *****************************************************************************
+// *****************************************************************************
 
 
 
