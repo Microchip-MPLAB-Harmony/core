ad2edd7b33309cf9097fcb32d8b22b3fb07b2f5a
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-04-20 17:08:15 +0530

    [DRV_I2C] Regenerated demo for driver_i2c_sync_i2c_eeprom_sam_c21n_xpro_freertos
    MH3-34500
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_c21n_xpro_freertos/peripheral/port/plib_port.c
+++ apps/driver/i2c/sync/i2c_eeprom/firmware/src/config/sam_c21n_xpro_freertos/peripheral/port/plib_port.c
@@ -76,14 +76,17 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[17] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000;
    PORT_REGS->GROUP[1].PORT_PINCFG[19] = 0x6;
 
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x20;
    PORT_REGS->GROUP[2].PORT_OUT = 0x20;
 
+                
 }
 
 // *****************************************************************************
