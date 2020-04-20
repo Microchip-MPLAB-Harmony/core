5c8e0bcae86c6f2018f23c128378b5c644d09bc3
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-04-20 17:29:38 +0530

    [DRV_SDSPI] Regenerated demo for driver_sdspi_async_sdspi_read_write_sam_d21_xpro
    MH3-34675
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/sdspi/async/sdspi_read_write/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
+++ apps/driver/sdspi/async/sdspi_read_write/firmware/src/config/sam_d21_xpro/peripheral/port/plib_port.c
@@ -81,10 +81,12 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[2] = 0x3;
    PORT_REGS->GROUP[0].PORT_PMUX[3] = 0x33;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_DIR = 0x40000000;
    PORT_REGS->GROUP[1].PORT_OUT = 0x40000000;
 
+                
 }
 
 // *****************************************************************************
