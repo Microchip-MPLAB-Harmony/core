ba01686c7e6c5a54cf040573acd5a10048c3ffc5
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-04-20 18:02:43 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_self_loopback_multi_client_sam_da1_xpro
    MH3-34753
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.h
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_da1_xpro/peripheral/port/plib_port.h
@@ -65,8 +65,7 @@
 // *****************************************************************************
 // *****************************************************************************
 
-
-
+  
 /*** Macros for CLIENT1_CS pin ***/
 #define CLIENT1_CS_Set()               (PORT_REGS->GROUP[0].PORT_OUTSET = 1 << 20)
 #define CLIENT1_CS_Clear()             (PORT_REGS->GROUP[0].PORT_OUTCLR = 1 << 20)
