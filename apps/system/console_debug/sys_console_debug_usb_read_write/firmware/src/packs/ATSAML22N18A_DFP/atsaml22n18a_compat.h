88d7d630a2a703405a56e7c16c7c8b2d9ea18330
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-04-20 18:55:53 +0530

    [SYS_CONSOLE_DEBUG] Regenerated demo for system_console_debug_sys_console_debug_usb_read_write_sam_l22_xpro
    MH3-35805
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/packs/ATSAML22N18A_DFP/atsaml22n18a_compat.h
+++ apps/system/console_debug/sys_console_debug_usb_read_write/firmware/src/packs/ATSAML22N18A_DFP/atsaml22n18a_compat.h
@@ -1,22 +1,26 @@
 /*******************************************************************************
-  USB stack external dependencies file
+  Device compatibility Header File
 
   Company:
     Microchip Technology Inc.
 
   File Name:
-    usb_external_dependencies.h
+    atsaml22n18a_compat.h
 
   Summary:
-    USB stack external dependencies file
+    This file includes deprecated macro names that are retained for the purpose
+    of compatibility with Harmony 3 Middleware. Users are discouraged from using
+    macros defined in this file. Recommend to use macros defined in the device
+    header file instead.
 
   Description:
-    USB stack external dependencies file. 
+    None
+
 *******************************************************************************/
 
-//DOM-IGNORE-BEGIN
+// DOM-IGNORE-BEGIN
 /*******************************************************************************
-* Copyright (C) 2018 Microchip Technology Inc. and its subsidiaries.
+* Copyright (C) 2020 Microchip Technology Inc. and its subsidiaries.
 *
 * Subject to your compliance with these terms, you may use Microchip software
 * and any derivatives exclusively with Microchip products. It is your
@@ -36,32 +40,25 @@
 * FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL CLAIMS IN
 * ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT OF FEES, IF ANY,
 * THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS SOFTWARE.
- *******************************************************************************/
-//DOM-IGNORE-END
+*******************************************************************************/
+// DOM-IGNORE-END
 
-#ifndef _USB_EXTERNAL_DEPENDENCIES_H
-#define _USB_EXTERNAL_DEPENDENCIES_H
+#ifndef ATSAML22N18A_COMPAT_H
 
-#include <string.h>
-#include "system/system_common.h"
-#include "configuration.h"
-#include "system/system_module.h"
+#ifndef DEVICE_DESC_BANK_NUMBER
+#define DEVICE_DESC_BANK_NUMBER USB_DEVICE_DESC_BANK_NUMBER
+#endif //DEVICE_DESC_BANK_NUMBER
 
-#if defined (USB_HOST_DEVICES_NUMBER) &&  (USB_HOST_DEVICES_NUMBER > 0)
-#include "system/time/sys_time.h"
-#define SYS_TMR_HANDLE_INVALID SYS_TIME_HANDLE_INVALID
-#define SYS_TMR_CallbackSingle(delay,context,callback) SYS_TIME_CallbackRegisterMS(callback,context,delay, SYS_TIME_SINGLE)
-#endif 
+#ifndef HOST_DESC_BANK_NUMBER
+#define HOST_DESC_BANK_NUMBER USB_HOST_DESC_BANK_NUMBER
+#endif //HOST_DESC_BANK_NUMBER
 
-#ifndef SYS_DEBUG_ENABLE
+#ifndef DEVICE_ENDPOINT_NUMBER
+#define DEVICE_ENDPOINT_NUMBER USB_DEVICE_ENDPOINT_NUMBER
+#endif //DEVICE_ENDPOINT_NUMBER
 
-	#define SYS_DEBUG_PRINT(level, format, ...) 
-	#define SYS_DEBUG_MESSAGE(a,b, ...)     
-	#define SYS_DEBUG(a,b)
-#else
-    #include "system/debug/sys_debug.h"
-#endif 
-#endif 
-/*******************************************************************************
- End of File
-*/
+#ifndef HOST_PIPE_NUMBER
+#define HOST_PIPE_NUMBER USB_HOST_PIPE_NUMBER
+#endif //HOST_PIPE_NUMBER
+
+#endif //ATSAML22N18A_COMPAT_H
