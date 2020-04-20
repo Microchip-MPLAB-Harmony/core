e334a45e7c39d5c6db4bb4e4278a7147936bdafb
Amit Raddi <amit.raddi@microchip.com>
2020-04-20 16:23:13 +0530

    [FREERTOS] Regenerated demo for rtos_freertos_task_notification_freertos_sam_e54_xpro
    MH3-35428
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/rtos/freertos/task_notification_freertos/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/rtos/freertos/task_notification_freertos/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -73,15 +73,19 @@ void PORT_Initialize(void)
 {
    /************************** GROUP 0 Initialization *************************/
 
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_PINCFG[31] = 0x4;
 
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
 
+                
    /************************** GROUP 3 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
