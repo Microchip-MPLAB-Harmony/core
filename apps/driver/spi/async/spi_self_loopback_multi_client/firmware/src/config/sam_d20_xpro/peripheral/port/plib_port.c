3452fbd2f1906c4d9d09c27e68eb136b152e3e9d
Vishal Gandhi <vishal.gandhi@microchip.com>
2020-04-20 17:19:48 +0530

    [DRV_SPI] Regenerated demo for driver_spi_async_spi_self_loopback_multi_client_sam_d20_xpro
    MH3-34749
    
    Signed-off-by: Vishal Gandhi <vishal.gandhi@microchip.com>

--- apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_d20_xpro/peripheral/port/plib_port.c
+++ apps/driver/spi/async/spi_self_loopback_multi_client/firmware/src/config/sam_d20_xpro/peripheral/port/plib_port.c
@@ -82,8 +82,10 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[0].PORT_PMUX[8] = 0x22;
    PORT_REGS->GROUP[0].PORT_PMUX[9] = 0x22;
+                
    /************************** GROUP 1 Initialization *************************/
 
+                
 }
 
 // *****************************************************************************
