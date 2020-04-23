586d8cb4d226dcfeb2ca457c3639a766e25b3336
Amit Raddi <amit.raddi@microchip.com>
2020-04-23 11:28:54 +0530

    [SYS_FS] Regenerated demo for fs_sdmmc_fat_sam_e54_xpro
    MH3-35235
    
    Signed-off-by: Amit Raddi <amit.raddi@microchip.com>

--- apps/fs/sdmmc_fat/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
+++ apps/fs/sdmmc_fat/firmware/src/config/sam_e54_xpro/peripheral/port/plib_port.c
@@ -76,6 +76,7 @@ void PORT_Initialize(void)
    PORT_REGS->GROUP[0].PORT_PINCFG[21] = 0x1;
 
    PORT_REGS->GROUP[0].PORT_PMUX[10] = 0x88;
+                
    /************************** GROUP 1 Initialization *************************/
    PORT_REGS->GROUP[1].PORT_OUT = 0x80000000;
    PORT_REGS->GROUP[1].PORT_PINCFG[18] = 0x1;
@@ -86,15 +87,18 @@ void PORT_Initialize(void)
 
    PORT_REGS->GROUP[1].PORT_PMUX[9] = 0x88;
    PORT_REGS->GROUP[1].PORT_PMUX[10] = 0x88;
+                
    /************************** GROUP 2 Initialization *************************/
    PORT_REGS->GROUP[2].PORT_DIR = 0x40000;
    PORT_REGS->GROUP[2].PORT_OUT = 0x40000;
 
+                
    /************************** GROUP 3 Initialization *************************/
    PORT_REGS->GROUP[3].PORT_PINCFG[20] = 0x1;
    PORT_REGS->GROUP[3].PORT_PINCFG[21] = 0x1;
 
    PORT_REGS->GROUP[3].PORT_PMUX[10] = 0x88;
+                
 }
 
 // *****************************************************************************
--- apps/fs/sdmmc_fat/firmware/src/config/sam_e54_xpro/tasks.c
+++ apps/fs/sdmmc_fat/firmware/src/config/sam_e54_xpro/tasks.c
@@ -55,6 +55,7 @@
 
 
 
+
 // *****************************************************************************
 // *****************************************************************************
 // Section: System "Tasks" Routine
@@ -89,6 +90,7 @@ DRV_SDMMC_Tasks(sysObj.drvSDMMC0);
     /* Maintain the application's state machine. */
         /* Call Application task APP. */
     APP_Tasks();
+
 
 
 
